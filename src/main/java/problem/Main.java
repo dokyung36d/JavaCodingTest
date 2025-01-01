3a1e9ed287199e4a0436a4dd4f8a1790b4278f77
dokyu <dokyung36@naver.com>
2025-01-01 13:19:46 +0900

    swim contest solved

--- src/main/java/problem/Main.java
+++ src/main/java/problem/Main.java
@@ -4,13 +4,20 @@ import java.util.*;
 import java.io.*;
 
 public class Main {
-    static class Node {
-        int curIndex;
-        int totalHeight;
+    public static class Node {
+        int depth;
+        int row;
+        int col;
 
-        public Node(int curIndex, int firstHeight) {
-            this.curIndex = curIndex;
-            this.totalHeight = firstHeight;
+
+        public Node(int depth, int row, int col) {
+            this.depth = depth;
+            this.row = row;
+            this.col = col;
+        }
+
+        public Node addNode(Node anotherNode) {
+            return new Node(this.depth + 1, this.row + anotherNode.row, this.col + anotherNode.col);
         }
     }
     public static void main(String[] args) throws IOException {
@@ -27,42 +34,70 @@ public class Main {
     public static int solution(BufferedReader br) throws IOException {
         StringTokenizer st = new StringTokenizer(br.readLine());
         int N = Integer.parseInt(st.nextToken());
-        int M = Integer.parseInt(st.nextToken());
+        int[][] matrix = new int[N][N];
+        Map<List<Integer>, Integer> visited = new HashMap<>();
 
-        st = new StringTokenizer(br.readLine());
-        int[] heightList = new int[N];
         for (int i = 0; i < N; i++) {
-            heightList[i] = Integer.parseInt(st.nextToken());
+            st = new StringTokenizer(br.readLine());
+            for (int j = 0; j < N; j++) {
+                matrix[i][j] = Integer.parseInt(st.nextToken());
+            }
         }
-        Deque<Node> stack = new ArrayDeque<>();
-        stack.addLast(new Node(0, 0));
-        stack.addLast(new Node(0, heightList[0]));
+        int[] startPos = new int[2];
+        int[] destPos = new int[2];
+        st = new StringTokenizer(br.readLine());
+        for (int i =0 ; i < 2; i++) {
+            startPos[i] = Integer.parseInt(st.nextToken());
+        }
+        st = new StringTokenizer(br.readLine());
+        for (int i =0 ; i < 2; i++) {
+            destPos[i] = Integer.parseInt(st.nextToken());
+        }
+        int[][] directions = {{-1, 0}, {1, 0}, {0, -1}, {0, 1}};
 
-        int minValue = (int) Math.pow(10, 5);
-        while (!stack.isEmpty()) {
-            Node node = stack.pollFirst();
-            if (node.totalHeight >= M) {
-                if (node.totalHeight - M < minValue) {
-                    minValue = node.totalHeight - M;
+        Deque<Node> queue = new ArrayDeque<>();
+        queue.add(new Node(0,startPos[0], startPos[1]));
+
+        while (!queue.isEmpty()) {
+            Node node = queue.pollFirst();
+            //System.out.println(Arrays.asList(node.row, node.col));
+
+            for (int[] direction : directions) {
+                Node movedNode = node.addNode(new Node(0, direction[0], direction[1]));
+                if (!checkIndex(movedNode, N)) {
+                    continue;
                 }
-                continue;
+
+                if (visited.get(Arrays.asList(movedNode.row, movedNode.col)) != null) {
+                    continue;
+                }
+
+                if (matrix[movedNode.row][movedNode.col] == 1) {
+                    continue;
+                }
+
+                if (matrix[movedNode.row][movedNode.col] == 2 && movedNode.depth % 3 != 0) {
+                    queue.addLast(new Node(node.depth + 1, node.row, node.col));
+                    continue;
+                }
+
+                if (movedNode.row == destPos[0] && movedNode.col == destPos[1]) {
+                    return movedNode.depth;
+                }
+                queue.addLast(movedNode);
+                visited.put(Arrays.asList(movedNode.row, movedNode.col), 1);
             }
-
-            if (node.curIndex == N - 1) {
-                continue;
-            }
-
-            Node nodeHeightNotAdded = new Node(node.curIndex + 1, node.totalHeight);
-            stack.addLast(nodeHeightNotAdded);
-
-            Node nodeHeightAdded = new Node(node.curIndex + 1, node.totalHeight + heightList[node.curIndex + 1]);
-            if (nodeHeightAdded.totalHeight - M > minValue) {
-                continue;
-            }
-            stack.addLast(nodeHeightAdded);
-
         }
 
-        return minValue;
+
+        return -1;
+    }
+
+    public static boolean checkIndex(Node node, int N) {
+        if (0 <= node.row && node.row < N && 0 <= node.col && node.col < N) {
+            return true;
+        }
+
+        return false;
     }
 }
