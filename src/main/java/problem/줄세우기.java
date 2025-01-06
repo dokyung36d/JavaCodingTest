657024fe7ac3a4d690e4fe96462c0e15192e982e
dokyu <dokyung36@naver.com>
2025-01-06 09:44:58 +0900

    line arrange solved

--- src/main/java/problem/줄세우기.java
+++ src/main/java/problem/줄세우기.java
@@ -7,7 +7,7 @@ import java.io.OutputStreamWriter;
 import java.io.BufferedWriter;
 import java.util.*;
 
-public class Main {
+public class 줄세우기 {
     static int N;
     static int[] numList;
     public static void main(String[] args) throws IOException {
