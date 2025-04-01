4c6ac3408074e0d4ac7468e05000c23931129a90
dokyu <dokyung36@naver.com>
2025-04-01 09:16:35 +0900

    고대 문명 유적 탐사 solved

--- src/main/java/problem/Main.java
+++ src/main/java/problem/Main.java
@@ -5,13 +5,12 @@ import java.io.*;
 
 
 public class Main {
-    static int R, C;
-    static char[][] mainMatrix;
-    static Pos[] directions = {new Pos(-1, 1), new Pos(0, 1), new Pos(1, 1)};
-    static int[][] visited;
-    static int answer = 0;
+    static int[][] mainMatrix;
+    static int K, M;
+    static Deque<Integer> treasureQueue;
+    static Pos[] directions = {new Pos(-1, 0), new Pos(0, 1), new Pos(1, 0), new Pos(0, - 1)};
 
-    public static class Pos {
+    public static class Pos implements Comparable<Pos> {
         int row;
         int col;
 
@@ -25,59 +24,212 @@ public class Main {
         }
 
         public boolean isValidIndex() {
-            if (this.row < 0 || this.row >= R || this.col < 0 || this.col >= C) { return false; }
+            if (this.row < 0 || this.row >= 5 || this.col < 0 || this.col >= 5) {
+                return false;
+            }
 
             return true;
         }
+
+        @Override
+        public int compareTo(Pos anotherPos) {
+            if (this.col != anotherPos.col) {
+                return Integer.compare(this.col, anotherPos.col);
+            }
+
+            return Integer.compare(-this.row, -anotherPos.row);
+        }
+    }
+
+    public static class Node implements Comparable<Node> {
+        PriorityQueue<Pos> poppedTreasurePriorityQueue;
+        int numRotated;
+        Pos topLeftPos;
+        int[][] partMatrix;
+
+        public Node(PriorityQueue<Pos> poppedTreasurePriorityQueue, int numRotated, Pos topLeftPos, int[][] partMatrix) {
+            this.poppedTreasurePriorityQueue = poppedTreasurePriorityQueue;
+            this.numRotated = numRotated;
+            this.topLeftPos = topLeftPos;
+            this.partMatrix = partMatrix;
+        }
+
+        @Override
+        public int compareTo(Node anotherNode) {
+            if (this.poppedTreasurePriorityQueue.size() != anotherNode.poppedTreasurePriorityQueue.size()) {
+                return Integer.compare(this.poppedTreasurePriorityQueue.size(), anotherNode.poppedTreasurePriorityQueue.size());
+            }
+
+            if (this.numRotated != anotherNode.numRotated) {
+                return Integer.compare(-this.numRotated, -anotherNode.numRotated);
+            }
+
+            if (this.topLeftPos.col != anotherNode.topLeftPos.col) {
+                return Integer.compare(-this.topLeftPos.col, -anotherNode.topLeftPos.col);
+            }
+
+            return Integer.compare(-this.topLeftPos.row, -anotherNode.topLeftPos.row);
+        }
+    }
+
+    public static int[][] getPartMatrix(Pos topLeftPos) {
+        int[][] partMatrix = new int[3][3];
+
+        for (int i = 0; i < 3; i++) {
+            for (int j = 0; j < 3; j++) {
+                partMatrix[i][j] = mainMatrix[topLeftPos.row + i][topLeftPos.col + j];
+            }
+        }
+
+        return partMatrix;
+    }
+
+    public static int[][] rotateMatrix(int[][] matrix) {
+        int[][] rotatedMatrix = new int[3][3];
+
+        for (int i = 0; i < 3; i++) {
+            for (int j = 0; j < 3; j++) {
+                rotatedMatrix[j][3 - i - 1] = matrix[i][j];
+            }
+        }
+
+        return rotatedMatrix;
+    }
+
+    public static int[][] applyPartMatrixToMainMatrix(int[][] partMatrix, Pos topLeftPos) {
+        int[][] appliedMatrix = new int[5][5];
+        for (int i = 0; i < 5; i++) {
+            for (int j = 0; j < 5; j++) {
+                appliedMatrix[i][j] = mainMatrix[i][j];
+            }
+        }
+
+        for (int i = 0; i < 3; i++) {
+            for (int j = 0; j < 3; j++) {
+                appliedMatrix[topLeftPos.row + i][topLeftPos.col + j] = partMatrix[i][j];
+            }
+        }
+
+        return appliedMatrix;
     }
 
     public static void main(String[] args) throws Exception {
         init();
 
-        answer = 0;
-        for (int i = 0; i < R; i++) {
-            dfs(new Pos(i, 0));
-        }
-        System.out.println(answer);
+        StringBuilder sb = new StringBuilder();
+        for (int k = 0; k < K; k++) {
+            int answer = 0;
 
+            Node bestNode = getBestNode();
+            if (bestNode.poppedTreasurePriorityQueue.size() < 3) {
+                break;
+            }
+
+            answer += bestNode.poppedTreasurePriorityQueue.size();
+            mainMatrix = applyPartMatrixToMainMatrix(bestNode.partMatrix, bestNode.topLeftPos);
+            putTreasureToMainMatrix(bestNode.poppedTreasurePriorityQueue);
+
+            while (true) {
+                PriorityQueue<Pos> priorityQueue = searchTreasure(mainMatrix);
+                if (priorityQueue.size() < 3) {
+                    break;
+                }
+
+                answer += priorityQueue.size();
+                putTreasureToMainMatrix(priorityQueue);
+            }
+            sb.append(answer + " ");
+        }
+
+        System.out.println(sb.toString().substring(0, sb.length() - 1));
     }
 
-    public static boolean dfs(Pos curPos) {
-        if (curPos.col == C - 1) {
-            answer += 1;
-            return true;
+    public static void putTreasureToMainMatrix(PriorityQueue<Pos> posPriorityQueue) {
+        while (!posPriorityQueue.isEmpty()) {
+            Pos pos = posPriorityQueue.poll();
+            int value = treasureQueue.pollFirst();
+
+            mainMatrix[pos.row][pos.col] = value;
         }
+    }
 
-        for (Pos direction : directions) {
-            Pos movedPos = curPos.addPos(direction);
-            if (!movedPos.isValidIndex()) { continue; }
-            if (mainMatrix[movedPos.row][movedPos.col] == 'x') { continue; }
-            if (visited[movedPos.row][movedPos.col] == 1) { continue; }
+    public static Node getBestNode() {
+        Node bestNode = new Node(new PriorityQueue<>(), 4, new Pos(3, 3), new int[3][3]);
 
-            visited[movedPos.row][movedPos.col] = 1;
-            if (dfs(movedPos)) {
-                return true;
+        for (int i = 0; i < 3; i++) {
+            for (int j = 0; j < 3; j++) {
+                Pos topLeftPos = new Pos(i, j);
+                int[][] partMatrix = getPartMatrix(topLeftPos);
+                for (int numRotated = 1; numRotated <= 3; numRotated++) {
+                    partMatrix = rotateMatrix(partMatrix);
+                    int[][] appliedMatrix = applyPartMatrixToMainMatrix(partMatrix, topLeftPos);
+                    PriorityQueue<Pos> searchedTreasurePriorityQueue = searchTreasure(appliedMatrix);
+                    Node node = new Node(searchedTreasurePriorityQueue, numRotated, topLeftPos, partMatrix);
+                    if (node.compareTo(bestNode) == 1) {
+                        bestNode = node;
+                    }
+                }
             }
         }
 
-        return false;
+        return bestNode;
+    }
+
+    public static PriorityQueue<Pos> searchTreasure(int[][] matrix) {
+        int[][] visited = new int[5][5];
+        PriorityQueue<Pos> priorityQueue = new PriorityQueue();
+
+        for (int i = 0; i < 5; i++) {
+            for (int j = 0; j < 5; j++) {
+                if (visited[i][j] == 1) { continue; }
+
+                List<Pos> treasureList = new ArrayList<>();
+                Deque<Pos> queue = new ArrayDeque<>();
+                queue.addLast(new Pos(i, j));
+                while (!queue.isEmpty()) {
+                    Pos curPos = queue.pollFirst();
+                    if (visited[curPos.row][curPos.col] == 1) { continue; }
+                    visited[curPos.row][curPos.col] = 1;
+                    treasureList.add(curPos);
+
+                    for (Pos direction : directions) {
+                        Pos movedPos = curPos.addPos(direction);
+                        if (!movedPos.isValidIndex() || visited[movedPos.row][movedPos.col] == 1) { continue; }
+                        if (matrix[curPos.row][curPos.col] != matrix[movedPos.row][movedPos.col]) { continue; }
+
+                        queue.addLast(movedPos);
+                    }
+                }
+
+                if (treasureList.size() >= 3) {
+                    priorityQueue.addAll(treasureList);
+                }
+            }
+        }
+
+        return priorityQueue;
     }
 
     public static void init() throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine());
 
-        R = Integer.parseInt(st.nextToken());
-        C = Integer.parseInt(st.nextToken());
+        K = Integer.parseInt(st.nextToken());
+        M = Integer.parseInt(st.nextToken());
 
-        mainMatrix = new char[R][C];
-        visited = new int[R][C];
-        for (int i = 0; i < R; i++) {
+        mainMatrix = new int[5][5];
+        for (int i = 0; i < 5; i++) {
             st = new StringTokenizer(br.readLine());
-            String string = st.nextToken();
-            for (int j = 0; j < C; j++) {
-                mainMatrix[i][j] = string.charAt(j);
+            for (int j = 0; j < 5; j++) {
+                mainMatrix[i][j] = Integer.parseInt(st.nextToken());
             }
         }
+
+        treasureQueue = new ArrayDeque<>();
+        st = new StringTokenizer(br.readLine());
+        for (int i = 0; i < M; i++) {
+            treasureQueue.addLast(Integer.parseInt(st.nextToken()));
+        }
+
     }
 }
--- src/main/java/problem/고대문명유적탐사.java
+++ src/main/java/problem/고대문명유적탐사.java
@@ -5,58 +5,12 @@ import java.io.*;
 
 
 public class 고대문명유적탐사 {
-    public static int K, M;
-    public static int[][] matrix;
-    public static Deque<Integer> treasureQueue;
-    public static Pos[] directions = {new Pos(-1, 0), new Pos(1, 0), new Pos(0, -1), new Pos(0, 1)};
+    static int[][] mainMatrix;
+    static int K, M;
+    static Deque<Integer> treasureQueue;
+    static Pos[] directions = {new Pos(-1, 0), new Pos(0, 1), new Pos(1, 0), new Pos(0, - 1)};
 
-    public static class MatrixNode implements Comparable<MatrixNode> {
-        List<Pos> poppedTreasureList;
-        int numRotated;
-        Pos topLeftPos;
-
-        public MatrixNode(List<Pos> poppedTreasureList, int numRotated, Pos topLeftPos) {
-            this.poppedTreasureList = poppedTreasureList;
-            this.numRotated = numRotated;
-            this.topLeftPos = topLeftPos;
-        }
-
-        @Override
-        public int compareTo(MatrixNode anotherMatrixNode) {
-            if (this.poppedTreasureList.size() != anotherMatrixNode.poppedTreasureList.size()) {
-                return Integer.compare(-this.poppedTreasureList.size(), -anotherMatrixNode.poppedTreasureList.size());
-            }
-
-            if (this.numRotated != anotherMatrixNode.numRotated) {
-                return Integer.compare(this.numRotated, anotherMatrixNode.numRotated);
-            }
-
-            if (this.topLeftPos.col != anotherMatrixNode.topLeftPos.col) {
-                return Integer.compare(this.topLeftPos.col, anotherMatrixNode.topLeftPos.col);
-            }
-
-            return Integer.compare(this.topLeftPos.row, anotherMatrixNode.topLeftPos.row);
-        }
-    }
-
-    public static class Treasure implements Comparable<Treasure> {
-        Pos pos;
-
-        public Treasure(Pos pos) {
-            this.pos = pos;
-        }
-
-        @Override
-        public int compareTo(Treasure anotherTreasure) {
-            if (this.pos.col == anotherTreasure.pos.col) {
-                return Integer.compare(-this.pos.row, -anotherTreasure.pos.row);
-            }
-
-            return Integer.compare(this.pos.col, anotherTreasure.pos.col);
-        }
-    }
-
-    public static class Pos {
+    public static class Pos implements Comparable<Pos> {
         int row;
         int col;
 
@@ -65,8 +19,8 @@ public class 고대문명유적탐사 {
             this.col = col;
         }
 
-        public Pos addPos(Pos anotherPos) {
-            return new Pos(this.row + anotherPos.row, this.col + anotherPos.col);
+        public Pos addPos(Pos direction) {
+            return new Pos(this.row + direction.row, this.col + direction.col);
         }
 
         public boolean isValidIndex() {
@@ -77,191 +31,183 @@ public class 고대문명유적탐사 {
             return true;
         }
 
-        public boolean isSubMatrixTopLeftPosPossible() {
-            if (this.row < 0 || this.row >= 4 || this.col < 0 || this.col >= 4) {
-                return false;
+        @Override
+        public int compareTo(Pos anotherPos) {
+            if (this.col != anotherPos.col) {
+                return Integer.compare(this.col, anotherPos.col);
             }
 
-            return true;
+            return Integer.compare(-this.row, -anotherPos.row);
         }
     }
 
+    public static class Node implements Comparable<Node> {
+        PriorityQueue<Pos> poppedTreasurePriorityQueue;
+        int numRotated;
+        Pos topLeftPos;
+        int[][] partMatrix;
 
+        public Node(PriorityQueue<Pos> poppedTreasurePriorityQueue, int numRotated, Pos topLeftPos, int[][] partMatrix) {
+            this.poppedTreasurePriorityQueue = poppedTreasurePriorityQueue;
+            this.numRotated = numRotated;
+            this.topLeftPos = topLeftPos;
+            this.partMatrix = partMatrix;
+        }
+
+        @Override
+        public int compareTo(Node anotherNode) {
+            if (this.poppedTreasurePriorityQueue.size() != anotherNode.poppedTreasurePriorityQueue.size()) {
+                return Integer.compare(this.poppedTreasurePriorityQueue.size(), anotherNode.poppedTreasurePriorityQueue.size());
+            }
+
+            if (this.numRotated != anotherNode.numRotated) {
+                return Integer.compare(-this.numRotated, -anotherNode.numRotated);
+            }
+
+            if (this.topLeftPos.col != anotherNode.topLeftPos.col) {
+                return Integer.compare(-this.topLeftPos.col, -anotherNode.topLeftPos.col);
+            }
+
+            return Integer.compare(-this.topLeftPos.row, -anotherNode.topLeftPos.row);
+        }
+    }
+
+    public static int[][] getPartMatrix(Pos topLeftPos) {
+        int[][] partMatrix = new int[3][3];
+
+        for (int i = 0; i < 3; i++) {
+            for (int j = 0; j < 3; j++) {
+                partMatrix[i][j] = mainMatrix[topLeftPos.row + i][topLeftPos.col + j];
+            }
+        }
+
+        return partMatrix;
+    }
+
+    public static int[][] rotateMatrix(int[][] matrix) {
+        int[][] rotatedMatrix = new int[3][3];
+
+        for (int i = 0; i < 3; i++) {
+            for (int j = 0; j < 3; j++) {
+                rotatedMatrix[j][3 - i - 1] = matrix[i][j];
+            }
+        }
+
+        return rotatedMatrix;
+    }
+
+    public static int[][] applyPartMatrixToMainMatrix(int[][] partMatrix, Pos topLeftPos) {
+        int[][] appliedMatrix = new int[5][5];
+        for (int i = 0; i < 5; i++) {
+            for (int j = 0; j < 5; j++) {
+                appliedMatrix[i][j] = mainMatrix[i][j];
+            }
+        }
+
+        for (int i = 0; i < 3; i++) {
+            for (int j = 0; j < 3; j++) {
+                appliedMatrix[topLeftPos.row + i][topLeftPos.col + j] = partMatrix[i][j];
+            }
+        }
+
+        return appliedMatrix;
+    }
 
     public static void main(String[] args) throws Exception {
         init();
 
-//        int[][] matrix = {{7, 5, 1, 7, 7}, {6, 7, 6, 7, 6}, {6, 6, 7, 6, 4}, {7, 6, 3, 2, 1}, {5, 4, 3, 2, 7}};
-//        List<Pos> posList = bfs(matrix);
-//        System.out.println("hello world");
-
         StringBuilder sb = new StringBuilder();
-
         for (int k = 0; k < K; k++) {
-            int value = 0;
-            MatrixNode bestMatrixNode = getBestMatrixNode();
-            if (bestMatrixNode.poppedTreasureList.size() == 0) { break; }
+            int answer = 0;
 
-            value += bestMatrixNode.poppedTreasureList.size();
-
-            int[][] subMatrix = getSubMatrix(bestMatrixNode.topLeftPos);
-            for (int i = 0; i < bestMatrixNode.numRotated; i++) {
-                subMatrix = rotateMatrix(subMatrix);
+            Node bestNode = getBestNode();
+            if (bestNode.poppedTreasurePriorityQueue.size() < 3) {
+                break;
             }
-            matrix = applySubMatrixToMatrix(bestMatrixNode.topLeftPos, subMatrix);
-            applyPoppedTreasureToMatrix(bestMatrixNode.poppedTreasureList);
+
+            answer += bestNode.poppedTreasurePriorityQueue.size();
+            mainMatrix = applyPartMatrixToMainMatrix(bestNode.partMatrix, bestNode.topLeftPos);
+            putTreasureToMainMatrix(bestNode.poppedTreasurePriorityQueue);
 
             while (true) {
-                List<Pos> poppedTreasureList = bfs(matrix);
-                if (poppedTreasureList.size() == 0) { break; }
+                PriorityQueue<Pos> priorityQueue = searchTreasure(mainMatrix);
+                if (priorityQueue.size() < 3) {
+                    break;
+                }
 
-                value += poppedTreasureList.size();
-                applyPoppedTreasureToMatrix(poppedTreasureList);
+                answer += priorityQueue.size();
+                putTreasureToMainMatrix(priorityQueue);
             }
-
-            sb.append(value + " ");
+            sb.append(answer + " ");
         }
 
         System.out.println(sb.toString().substring(0, sb.length() - 1));
     }
 
-    public static void applyPoppedTreasureToMatrix(List<Pos> poppedTreasureList) {
-        PriorityQueue<Treasure> pq = new PriorityQueue<>();
-        for (Pos poppedTreasure : poppedTreasureList) {
-            pq.add(new Treasure(poppedTreasure));
-        }
+    public static void putTreasureToMainMatrix(PriorityQueue<Pos> posPriorityQueue) {
+        while (!posPriorityQueue.isEmpty()) {
+            Pos pos = posPriorityQueue.poll();
+            int value = treasureQueue.pollFirst();
 
-        while (!pq.isEmpty()) {
-            Treasure treasure = pq.poll();
-            matrix[treasure.pos.row][treasure.pos.col] = treasureQueue.pollFirst();
+            mainMatrix[pos.row][pos.col] = value;
         }
     }
 
-    public static MatrixNode getBestMatrixNode() {
-        MatrixNode bestMatrixNode = new MatrixNode(new ArrayList<>(), 0, new Pos(0, 0));
+    public static Node getBestNode() {
+        Node bestNode = new Node(new PriorityQueue<>(), 4, new Pos(3, 3), new int[3][3]);
 
         for (int i = 0; i < 3; i++) {
             for (int j = 0; j < 3; j++) {
                 Pos topLeftPos = new Pos(i, j);
-                int[][] subMatrix = getSubMatrix(topLeftPos);
-
-                int[][] onceRotatedMatrix = rotateMatrix(subMatrix);
-                int[][] onceRotatedAppliedMatrix = applySubMatrixToMatrix(topLeftPos, onceRotatedMatrix);
-                List<Pos> onceRotatedPoppedTreasureList = bfs(onceRotatedAppliedMatrix);
-                MatrixNode onceRotatedMatrixNode = new MatrixNode(onceRotatedPoppedTreasureList, 1, topLeftPos);
-                if (onceRotatedMatrixNode.compareTo(bestMatrixNode) < 0) {
-                    bestMatrixNode = onceRotatedMatrixNode;
-                }
-
-                int[][] twiceRotatedMatrix = rotateMatrix(onceRotatedMatrix);
-                int[][] twiceRotatedAppliedMatrix = applySubMatrixToMatrix(topLeftPos, twiceRotatedMatrix);
-                List<Pos> twiceRotatedPoppedTreasureList = bfs(twiceRotatedAppliedMatrix);
-                MatrixNode twiceRotatedMatrixNode = new MatrixNode(twiceRotatedPoppedTreasureList, 2, topLeftPos);
-                if (twiceRotatedMatrixNode.compareTo(bestMatrixNode) < 0) {
-                    bestMatrixNode = twiceRotatedMatrixNode;
-                }
-
-                int[][] threeRotatedMatrix = rotateMatrix(twiceRotatedMatrix);
-                int[][] threeRotatedAppliedMatrix = applySubMatrixToMatrix(topLeftPos, threeRotatedMatrix);
-                List<Pos> threeRotatedPoppedTreasureList = bfs(threeRotatedAppliedMatrix);
-                MatrixNode threeRotatedMatrixNode = new MatrixNode(threeRotatedPoppedTreasureList, 3, topLeftPos);
-                if (threeRotatedMatrixNode.compareTo(bestMatrixNode) < 0) {
-                    bestMatrixNode = threeRotatedMatrixNode;
+                int[][] partMatrix = getPartMatrix(topLeftPos);
+                for (int numRotated = 1; numRotated <= 3; numRotated++) {
+                    partMatrix = rotateMatrix(partMatrix);
+                    int[][] appliedMatrix = applyPartMatrixToMainMatrix(partMatrix, topLeftPos);
+                    PriorityQueue<Pos> searchedTreasurePriorityQueue = searchTreasure(appliedMatrix);
+                    Node node = new Node(searchedTreasurePriorityQueue, numRotated, topLeftPos, partMatrix);
+                    if (node.compareTo(bestNode) == 1) {
+                        bestNode = node;
+                    }
                 }
             }
         }
 
-        return bestMatrixNode;
+        return bestNode;
     }
 
-    public static List<Pos> bfs(int[][] rotatedMatrix) {
+    public static PriorityQueue<Pos> searchTreasure(int[][] matrix) {
         int[][] visited = new int[5][5];
-        List<Pos> poppedTreasureList = new ArrayList<>();
+        PriorityQueue<Pos> priorityQueue = new PriorityQueue();
 
         for (int i = 0; i < 5; i++) {
             for (int j = 0; j < 5; j++) {
-                Pos pos = new Pos(i, j);
-                if (visited[pos.row][pos.col] == 1) { continue; }
+                if (visited[i][j] == 1) { continue; }
 
-                int treasureValue = rotatedMatrix[pos.row][pos.col];
-
+                List<Pos> treasureList = new ArrayList<>();
                 Deque<Pos> queue = new ArrayDeque<>();
-                queue.add(pos);
-                List<Pos> adjacentTreasureList = new ArrayList<>();
-
+                queue.addLast(new Pos(i, j));
                 while (!queue.isEmpty()) {
                     Pos curPos = queue.pollFirst();
                     if (visited[curPos.row][curPos.col] == 1) { continue; }
-
-                    adjacentTreasureList.add(curPos);
                     visited[curPos.row][curPos.col] = 1;
+                    treasureList.add(curPos);
+
                     for (Pos direction : directions) {
                         Pos movedPos = curPos.addPos(direction);
                         if (!movedPos.isValidIndex() || visited[movedPos.row][movedPos.col] == 1) { continue; }
-                        if (rotatedMatrix[movedPos.row][movedPos.col] != treasureValue) { continue; }
+                        if (matrix[curPos.row][curPos.col] != matrix[movedPos.row][movedPos.col]) { continue; }
 
-                        queue.add(movedPos);
+                        queue.addLast(movedPos);
                     }
                 }
 
-                if (adjacentTreasureList.size() < 3) { continue; }
-                for (Pos adjacentTreasure : adjacentTreasureList) {
-                    poppedTreasureList.add(adjacentTreasure);
+                if (treasureList.size() >= 3) {
+                    priorityQueue.addAll(treasureList);
                 }
             }
         }
 
-        return poppedTreasureList;
-    }
-
-    public static int[][] applySubMatrixToMatrix(Pos topLeftPos, int[][] subMatrix) {
-        int[][] appliedMatrix = new int[5][5];
-
-        int[][] copiedMatrix = copyMatrix(matrix);
-        for (int i = 0; i < 3; i++) {
-            for (int j = 0; j < 3; j++) {
-                Pos direction = new Pos(i, j);
-                copiedMatrix[topLeftPos.row + direction.row][topLeftPos.col + direction.col] = subMatrix[direction.row][direction.col];
-            }
-        }
-
-        return copiedMatrix;
-    }
-
-    public static int[][] copyMatrix(int[][] originalMatrix) {
-        int[][] copiedMatrix = new int[5][5];
-        for (int i = 0; i < originalMatrix.length; i++) {
-            for (int j = 0; j < originalMatrix.length; j++) {
-                copiedMatrix[i][j] = originalMatrix[i][j];
-            }
-        }
-
-        return copiedMatrix;
-    }
-
-    public static int[][] getSubMatrix(Pos topLeftPos) {
-        int[][] subMatrix = new int[3][3];
-
-        for (int i = 0; i <= 2; i++) {
-            for (int j = 0; j <= 2; j++) {
-                Pos direction = new Pos(i, j);
-                subMatrix[i][j] = matrix[topLeftPos.row + direction.row][topLeftPos.col + direction.col];
-            }
-        }
-
-        return subMatrix;
-    }
-
-    public static int[][] rotateMatrix(int[][] subMatrix) {
-        int[][] rotatedMatrix = new int[3][3];
-
-        for (int i = 0; i < 3; i++) {
-            for (int j = 0; j < 3; j++) {
-                rotatedMatrix[j][3 - i - 1] = subMatrix[i][j];
-            }
-        }
-
-        return rotatedMatrix;
+        return priorityQueue;
     }
 
     public static void init() throws IOException {
@@ -271,19 +217,19 @@ public class 고대문명유적탐사 {
         K = Integer.parseInt(st.nextToken());
         M = Integer.parseInt(st.nextToken());
 
-        matrix = new int[5][5];
+        mainMatrix = new int[5][5];
         for (int i = 0; i < 5; i++) {
             st = new StringTokenizer(br.readLine());
             for (int j = 0; j < 5; j++) {
-                matrix[i][j] = Integer.parseInt(st.nextToken());
+                mainMatrix[i][j] = Integer.parseInt(st.nextToken());
             }
         }
 
         treasureQueue = new ArrayDeque<>();
         st = new StringTokenizer(br.readLine());
         for (int i = 0; i < M; i++) {
-            int treasure = Integer.parseInt(st.nextToken());
-            treasureQueue.addLast(treasure);
+            treasureQueue.addLast(Integer.parseInt(st.nextToken()));
         }
+
     }
 }
