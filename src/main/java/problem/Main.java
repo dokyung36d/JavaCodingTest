d58ebb62689cc265d81c9aef474566687f5f5d16
dokyu <dokyung36@naver.com>
2025-06-03 11:55:20 +0900

    팰린드롬 solved

--- src/main/java/problem/Main.java
+++ src/main/java/problem/Main.java
@@ -5,130 +5,78 @@ import java.io.*;
 
 
 public class Main {
-	static String string1, string2;
-	static Pos upLeftDirection = new Pos(-1, -1);
-	static Pos upDirection = new Pos(-1, 0);
-	static Pos leftDirection = new Pos(0, -1);
-	static int[][] dpMatrix;
+	static int N;
+	static int[] numList;
+	static int M;
+	static Query[] queryList;
 
-	public static class Pos {
-		int row;
-		int col;
+	public static class Query {
+		int from;
+		int to;
 
-		public Pos(int row, int col) {
-			this.row = row;
-			this.col = col;
-		}
-
-		public Pos addPos(Pos direction) {
-			return new Pos(this.row + direction.row, this.col + direction.col);
-		}
-
-		public int getValue() {
-			return dpMatrix[this.row][this.col];
+		public Query(int from, int to) {
+			this.from = from;
+			this.to = to;
 		}
 	}
-
 	public static void main(String[] args) throws Exception {
 		init();
 		solution();
 	}
 
 	public static void solution() {
-		int M = string1.length();
-		int N = string2.length();
-		dpMatrix = new int[N][M];
-
-
-		int rowFlag = 0;
-		int colFlag = 0;
+		int[][] dpMatrix = new int[N][N];
 		for (int i = 0; i < N; i++) {
-			if (string1.charAt(0) == string2.charAt(i)) {
-				rowFlag = 1;
-			}
-			if (rowFlag == 1) {
-				dpMatrix[i][0] = 1;
+			dpMatrix[i][i] = 1;
+		}
+
+		for (int i = 0; i < N - 1; i++) {
+			if (numList[i] == numList[i + 1]) {
+				dpMatrix[i][i + 1] = 1;
 			}
 		}
 
-		for (int i = 0; i < M; i++) {
-			if (string1.charAt(i) == string2.charAt(0)) {
-				colFlag = 1;
-			}
-			if (colFlag == 1) {
-				dpMatrix[0][i] = 1;
-			}
-		}
-
-
-
-		for (int i = 1; i < N; i++) {
-			for (int j = 1; j < M; j++) {
-				if (string2.charAt(i) == string1.charAt(j)) {
-					dpMatrix[i][j] = dpMatrix[i - 1][j - 1] + 1;
-				}
-
-				else {
-					dpMatrix[i][j] = Math.max(dpMatrix[i - 1][j],
-							dpMatrix[i][j - 1]);
+		for (int gap = 2; gap <= N - 1; gap++) {
+			for (int start = 0; start + gap < N; start++) {
+				int end = start + gap;
+				if (numList[start] == numList[end] && dpMatrix[start + 1][end - 1] == 1) {
+					dpMatrix[start][end] = 1;
 				}
 			}
 		}
-
 
 		StringBuilder sb = new StringBuilder();
-		Pos curPos = new Pos(N - 1, M - 1);
-		while (curPos.row >= 1 && curPos.col >= 1) {
-			Pos upLeftPos = curPos.addPos(upLeftDirection);
-			Pos upPos = curPos.addPos(upDirection);
-			Pos leftPos = curPos.addPos(leftDirection);
-
-			if (curPos.getValue() == upLeftPos.getValue()) {
-				curPos = upLeftPos;
-				continue;
-			}
-
-			if (curPos.getValue() == leftPos.getValue()) {
-				curPos = leftPos;
-				continue;
-			}
-
-			if (curPos.getValue() == upPos.getValue()) {
-				curPos = upPos;
-				continue;
-			}
-
-			sb.append(string2.charAt(curPos.row));
-			curPos = upLeftPos;
+		for (int i = 0; i < M; i++) {
+			Query query = queryList[i];
+			sb.append(dpMatrix[query.from][query.to]);
+			sb.append("\n");
 		}
 
-
-		if (curPos.getValue() == 0) {
-
-		}
-
-		else if (curPos.row == 0) {
-			sb.append(string2.charAt(0));
-		}
-		else {
-			sb.append(string1.charAt(0));
-		}
-
-		if (dpMatrix[N - 1][M - 1] == 0) {
-			System.out.println(0);
-			return;
-		}
-		System.out.println(dpMatrix[N - 1][M - 1]);
-		System.out.println(sb.reverse().toString());
+		System.out.println(sb.toString().substring(0, sb.length() - 1));
 	}
 
 	public static void init() throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
 		StringTokenizer st = new StringTokenizer(br.readLine());
-		string1 = st.nextToken();
+		N = Integer.parseInt(st.nextToken());
+		numList = new int[N];
 
 		st = new StringTokenizer(br.readLine());
-		string2 = st.nextToken();
+		for (int i = 0; i < N; i++) {
+			numList[i] = Integer.parseInt(st.nextToken());
+		}
+
+		st = new StringTokenizer(br.readLine());
+		M = Integer.parseInt(st.nextToken());
+		queryList = new Query[M];
+
+		for (int i = 0; i < M; i++) {
+			st = new StringTokenizer(br.readLine());
+
+			int from = Integer.parseInt(st.nextToken()) - 1;
+			int to = Integer.parseInt(st.nextToken()) - 1;
+			queryList[i] = new Query(from, to);
+		}
 	}
 }
--- src/main/java/problem/펠린드롬.java
+++ src/main/java/problem/펠린드롬.java
@@ -5,51 +5,60 @@ import java.io.*;
 
 
 public class 펠린드롬 {
-    static int N, M;
-    static int[] numList, commandList;
+    static int N;
+    static int[] numList;
+    static int M;
+    static Main.Query[] queryList;
 
+    public static class Query {
+        int from;
+        int to;
+
+        public Query(int from, int to) {
+            this.from = from;
+            this.to = to;
+        }
+    }
     public static void main(String[] args) throws Exception {
         init();
+        solution();
+    }
 
+    public static void solution() {
         int[][] dpMatrix = new int[N][N];
-
         for (int i = 0; i < N; i++) {
             dpMatrix[i][i] = 1;
         }
+
         for (int i = 0; i < N - 1; i++) {
             if (numList[i] == numList[i + 1]) {
                 dpMatrix[i][i + 1] = 1;
             }
         }
 
-        for (int i = 2; i < N; i++) {
-            for (int j = 0; j < N - i; j++) {
-                int startIndex = j;
-                int endIndex = j + i;
-
-                if (numList[startIndex] == numList[endIndex]) {
-                    dpMatrix[j][j + i] = dpMatrix[j + 1][j + i - 1];
+        for (int gap = 2; gap <= N - 1; gap++) {
+            for (int start = 0; start + gap < N; start++) {
+                int end = start + gap;
+                if (numList[start] == numList[end] && dpMatrix[start + 1][end - 1] == 1) {
+                    dpMatrix[start][end] = 1;
                 }
             }
         }
 
         StringBuilder sb = new StringBuilder();
         for (int i = 0; i < M; i++) {
-            int startIndex = commandList[2 * i];
-            int endIndex = commandList[2 * i + 1];
-
-            sb.append(dpMatrix[startIndex][endIndex]);
+            Main.Query query = queryList[i];
+            sb.append(dpMatrix[query.from][query.to]);
             sb.append("\n");
         }
 
         System.out.println(sb.toString().substring(0, sb.length() - 1));
     }
 
-
     public static void init() throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        StringTokenizer st = new StringTokenizer(br.readLine());
 
+        StringTokenizer st = new StringTokenizer(br.readLine());
         N = Integer.parseInt(st.nextToken());
         numList = new int[N];
 
@@ -60,12 +69,14 @@ public class 펠린드롬 {
 
         st = new StringTokenizer(br.readLine());
         M = Integer.parseInt(st.nextToken());
-        commandList = new int[2 * M];
+        queryList = new Main.Query[M];
 
         for (int i = 0; i < M; i++) {
             st = new StringTokenizer(br.readLine());
-            commandList[2 * i] = Integer.parseInt(st.nextToken()) - 1;
-            commandList[2 * i + 1] = Integer.parseInt(st.nextToken()) - 1;
+
+            int from = Integer.parseInt(st.nextToken()) - 1;
+            int to = Integer.parseInt(st.nextToken()) - 1;
+            queryList[i] = new Main.Query(from, to);
         }
     }
 }
