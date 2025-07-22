875a42f092c891ce88c6042470803d6285042f2e
dokyu <dokyung36@naver.com>
2025-07-22 22:42:15 +0900

    뉴스 전하기 solved

--- src/main/java/problem/Main.java
+++ src/main/java/problem/Main.java
@@ -5,57 +5,21 @@ import java.io.*;
 
 
 public class Main {
-	static int N, M;
-	static int[][] wallMatrix;
-	static int[][] groupMatrix;
-	static Pos[] directions = {new Pos(0, -1), new Pos(-1, 0), new Pos(0, 1), new Pos(1, 0)};
+	static int N;
+	static int[] bossList, isParent;
+	static Map<Integer, List<Integer>> childMap;
 
-	public static class Pos {
-		int row;
-		int col;
+	public static class Node implements Comparable<Node> {
+		int curNum;
+		int time;
 
-		public Pos(int row, int col) {
-			this.row = row;
-			this.col = col;
+		public Node(int curNum, int time) {
+			this.curNum = curNum;
+			this.time = time;
 		}
 
-		public Pos addPos(Pos direction) {
-			return new Pos(this.row + direction.row, this.col + direction.col);
-		}
-
-		public boolean isValidIndex() {
-			if (this.row < 0 || this.row >= N || this.col < 0 || this.col >= M) { return false; }
-
-			return true;
-		}
-	}
-
-	public static class Neighbor {
-		int smallUniqueNum;
-		int bigUniqueNum;
-
-		public Neighbor(int uniqueNum1, int uniqueNum2) {
-			this.smallUniqueNum = Math.min(uniqueNum1, uniqueNum2);
-			this.bigUniqueNum = Math.max(uniqueNum1, uniqueNum2);
-		}
-
-
-		@Override
-		public boolean equals(Object obj) {
-			if (this == obj) { return true; }
-			if (obj == null || this.getClass() != obj.getClass()) { return false; }
-
-			Neighbor anotherNeighbor = (Neighbor) obj;
-			if (this.smallUniqueNum == anotherNeighbor.smallUniqueNum && this.bigUniqueNum == anotherNeighbor.bigUniqueNum) {
-				return true;
-			}
-
-			return false;
-		}
-
-		@Override
-		public int hashCode() {
-			return Objects.hash(this.smallUniqueNum, this.bigUniqueNum);
+		public int compareTo(Node anotherNode) {
+			return Integer.compare(-this.time, -anotherNode.time);
 		}
 	}
 
@@ -65,88 +29,71 @@ public class Main {
 	}
 
 	public static void solution() {
-		groupMatrix = new int[N][M];
+		if (N == 1) {
+			System.out.println(0);
+			return;
+		}
+		boolean result = doesChildHaveChild(1);
+		int answer = recursive(0);
+		System.out.println(answer);
+	}
 
-		int groupUniqueNum = 1;
-		for (int i = 0; i < N; i++) {
-			for (int j = 0; j < M; j++) {
-				if (groupMatrix[i][j] != 0) { continue; }
+	public static int recursive(int parentNum) {
+		if (!doesChildHaveChild(parentNum)) {
+			return childMap.get(parentNum).size();
+		}
 
-				Deque<Pos> queue = new ArrayDeque<>();
-				queue.add(new Pos(i, j));
+		int maxTime = 0;
+		PriorityQueue<Node> pq = new PriorityQueue<>();
+		for (int child : childMap.get(parentNum)) {
+			pq.add(new Node(child, recursive(child)));
+		}
 
-				while (!queue.isEmpty()) {
-					Pos curPos = queue.pollFirst();
-					if (groupMatrix[curPos.row][curPos.col] != 0) { continue; }
-					groupMatrix[curPos.row][curPos.col] = groupUniqueNum;
+		int calledTime = 0;
+		while (!pq.isEmpty()) {
+			Node node = pq.poll();
+			calledTime += 1;
 
-					for (int directionIndex = 0; directionIndex < 4; directionIndex++) {
-						if ((wallMatrix[curPos.row][curPos.col] & (1 << directionIndex)) != 0) { continue; }
-						Pos direction = directions[directionIndex];
+			maxTime = Math.max(maxTime, calledTime + node.time);
 
-						Pos movedPos = curPos.addPos(direction);
-						if (!movedPos.isValidIndex()) { continue; }
-						if (groupMatrix[movedPos.row][movedPos.col] != 0) { continue; }
+		}
 
-						queue.add(movedPos);
-					}
-				}
 
-				groupUniqueNum += 1;
+		return maxTime;
+	}
+
+	public static boolean doesChildHaveChild(int parentNum) {
+		List<Integer> childList = childMap.get(parentNum);
+
+		for (int child : childList) {
+			if (isParent[child] == 1) {
+				return true;
 			}
 		}
 
-
-		Map<Integer, Integer> groupSizeMap = new HashMap<>();
-		Map<Neighbor, Integer> neighborMap = new HashMap<>();
-		for (int i = 0; i < N; i++) {
-			for (int j = 0; j < M; j++) {
-				groupSizeMap.put(groupMatrix[i][j], groupSizeMap.getOrDefault(groupMatrix[i][j], 0) + 1);
-
-				for (int directionIndex = 0; directionIndex < 4; directionIndex++) {
-					if ((wallMatrix[i][j] & (1 << directionIndex)) == 0) { continue; }
-
-					Pos curPos = new Pos(i, j);
-					Pos movedPos = curPos.addPos(directions[directionIndex]);
-					if (!movedPos.isValidIndex()) { continue; }
-					if (groupMatrix[curPos.row][curPos.col] == groupMatrix[movedPos.row][movedPos.col]) { continue; }
-
-					neighborMap.put(new Neighbor(groupMatrix[curPos.row][curPos.col], groupMatrix[movedPos.row][movedPos.col]), 1);
-				}
-			}
-		}
-
-		StringBuilder sb = new StringBuilder();
-		sb.append(groupSizeMap.keySet().size());
-		sb.append("\n");
-
-		int maxGroupSize = 0;
-		for (int uniqueNum : groupSizeMap.keySet()) {
-			maxGroupSize = Math.max(maxGroupSize, groupSizeMap.get(uniqueNum));
-		}
-		sb.append(maxGroupSize);
-		sb.append("\n");
-
-		int maxSize = 0;
-		for (Neighbor neighbor : neighborMap.keySet()) {
-			maxSize = Math.max(maxSize, groupSizeMap.get(neighbor.smallUniqueNum) + groupSizeMap.get(neighbor.bigUniqueNum));
-		}
-		sb.append(maxSize);
-		System.out.println(sb.toString());
+		return false;
 	}
 
 	public static void init() throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		StringTokenizer st = new StringTokenizer(br.readLine());
 
-		M = Integer.parseInt(st.nextToken());
 		N = Integer.parseInt(st.nextToken());
+		bossList = new int[N];
+		isParent = new int[N];
 
-		wallMatrix = new int[N][M];
+		childMap = new HashMap<>();
 		for (int i = 0; i < N; i++) {
-			st = new StringTokenizer(br.readLine());
-			for (int j = 0; j < M; j++) {
-				wallMatrix[i][j] = Integer.parseInt(st.nextToken());
+			childMap.put(i, new ArrayList<>());
+		}
+
+		st = new StringTokenizer(br.readLine());
+		for (int i = 0; i < N; i++) {
+			bossList[i] = Integer.parseInt(st.nextToken());
+
+			if (i != 0) {
+				isParent[bossList[i]] = 1;
+				childMap.get(bossList[i]).add(i);
 			}
 		}
 	}
---
+++ src/main/java/problem/뉴스전하기.java
@@ -0,0 +1,100 @@
+package problem;
+
+import java.util.*;
+import java.io.*;
+
+
+public class 뉴스전하기 {
+    static int N;
+    static int[] bossList, isParent;
+    static Map<Integer, List<Integer>> childMap;
+
+    public static class Node implements Comparable<Node> {
+        int curNum;
+        int time;
+
+        public Node(int curNum, int time) {
+            this.curNum = curNum;
+            this.time = time;
+        }
+
+        public int compareTo(Node anotherNode) {
+            return Integer.compare(-this.time, -anotherNode.time);
+        }
+    }
+
+    public static void main(String[] args) throws Exception {
+        init();
+        solution();
+    }
+
+    public static void solution() {
+        if (N == 1) {
+            System.out.println(0);
+            return;
+        }
+        boolean result = doesChildHaveChild(1);
+        int answer = recursive(0);
+        System.out.println(answer);
+    }
+
+    public static int recursive(int parentNum) {
+        if (!doesChildHaveChild(parentNum)) {
+            return childMap.get(parentNum).size();
+        }
+
+        int maxTime = 0;
+        PriorityQueue<Node> pq = new PriorityQueue<>();
+        for (int child : childMap.get(parentNum)) {
+            pq.add(new Node(child, recursive(child)));
+        }
+
+        int calledTime = 0;
+        while (!pq.isEmpty()) {
+            Node node = pq.poll();
+            calledTime += 1;
+
+            maxTime = Math.max(maxTime, calledTime + node.time);
+
+        }
+
+
+        return maxTime;
+    }
+
+    public static boolean doesChildHaveChild(int parentNum) {
+        List<Integer> childList = childMap.get(parentNum);
+
+        for (int child : childList) {
+            if (isParent[child] == 1) {
+                return true;
+            }
+        }
+
+        return false;
+    }
+
+    public static void init() throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        N = Integer.parseInt(st.nextToken());
+        bossList = new int[N];
+        isParent = new int[N];
+
+        childMap = new HashMap<>();
+        for (int i = 0; i < N; i++) {
+            childMap.put(i, new ArrayList<>());
+        }
+
+        st = new StringTokenizer(br.readLine());
+        for (int i = 0; i < N; i++) {
+            bossList[i] = Integer.parseInt(st.nextToken());
+
+            if (i != 0) {
+                isParent[bossList[i]] = 1;
+                childMap.get(bossList[i]).add(i);
+            }
+        }
+    }
+}
