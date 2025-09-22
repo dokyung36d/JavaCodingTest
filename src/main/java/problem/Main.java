de2c5c67b9122478d3e7eb608ee5e52204efb159
dokyu <dokyung36@naver.com>
2025-09-22 22:07:58 +0900

    왕실의 기사 대결 solved

--- src/main/java/problem/Main.java
+++ src/main/java/problem/Main.java
@@ -4,23 +4,14 @@ import java.util.*;
 import java.io.*;
 
 public class Main {
-	static int N, M, P, C, D;
-	static Pos deerPos;
-	static int[][] mainMatrix;
-	static Pos[] deerDirections = {new Pos(-1, -1), new Pos(-1, 0), new Pos(-1, 1),
-			new Pos(0, -1), new Pos(0, 1),
-			new Pos(1, -1), new Pos(1, 0), new Pos(1, 1)};
-	static Pos[] santaDirections = {new Pos(-1, 0), new Pos(0, 1), new Pos(1, 0),
-			new Pos(0, -1)};
+	static int L, N, Q;
+	static int[][] mainMatrix, soldierMatrix;
+	static Pos[] directions = {new Pos(-1, 0), new Pos(0, 1), new Pos(1, 0), new Pos(0, -1)};
+	static Map<Integer, Soldier> soldierMap;
+	static int[][] commands;
+	static int[] initPList;
 	
-	static Map<Integer, Santa> santaMap;
-	static Map<Integer, Integer> santaOutedMap;
-	static int[] santaDowned;
-	
-	static int[] scoreList;
-	
-	
-	public static class Pos implements Comparable<Pos> {
+	public static class Pos {
 		int row;
 		int col;
 		
@@ -33,76 +24,80 @@ public class Main {
 			return new Pos(this.row + direction.row, this.col + direction.col);
 		}
 		
-		public Pos minusPos(Pos anotherPos) {
-			return new Pos(this.row - anotherPos.row, this.col - anotherPos.col);
-		}
-		
-		public Pos multiplyPos(int num) {
-			return new Pos(this.row * num, this.col * num);
-		}
-		
-		public Pos reversePos() {
-			return new Pos(-this.row, -this.col);
-		}
-		
 		public boolean isValidIndex() {
-			if (this.row < 0 || this.row >= N || this.col < 0 || this.col >= N) {
+			if (this.row < 0 || this.row >= L || this.col < 0 || this.col >= L) {
 				return false;
 			}
 			
 			return true;
 		}
-		
-		
-		public int calcDistance(Pos anotherPos) {
-			return (int) (Math.pow(this.row - anotherPos.row, 2) +
-					Math.pow(this.col - anotherPos.col, 2));
-		}
-		
-		@Override
-		public boolean equals(Object obj) {
-			if (this == obj) { return true; }
-			if (obj == null || this.getClass() != obj.getClass()) { return false; }
-			
-			Pos anotherPos = (Pos) obj;
-			if (this.row == anotherPos.row && this.col == anotherPos.col) { return true; }
-			return false;
-		}
-		
-		@Override
-		public int hashCode() {
-			return Objects.hash(this.row, this.col);
-		}
-		
-		@Override
-		public int compareTo(Pos anotherPos) {
-			if (this.calcDistance(deerPos) != anotherPos.calcDistance(deerPos)) {
-				return Integer.compare(this.calcDistance(deerPos),
-						anotherPos.calcDistance(deerPos));
-			}
-			
-			if (this.row != anotherPos.row) {
-				return Integer.compare(-this.row, -anotherPos.row);
-			}
-			
-			return Integer.compare(-this.col, -anotherPos.col);
-		}
 	}
 	
-	public static class Santa implements Comparable<Santa> {
-		int uniqueNum;
-		Pos curPos;
+	public static class Soldier {
+		int pk;
+		Pos topLeftPos;
+		int h;
+		int w;
+		int p;
 		
-		public Santa(int uniqueNum, Pos curPos) {
-			this.uniqueNum = uniqueNum;
-			this.curPos = curPos;
+		public Soldier(int pk, Pos topLeftPos, int h, int w, int p) {
+			this.pk = pk;
+			this.topLeftPos = topLeftPos;
+			this.h = h;
+			this.w = w;
+			this.p = p;
 		}
 		
-		
-		public int compareTo(Santa anotherSanta) {
-			return Integer.compare(this.uniqueNum, anotherSanta.uniqueNum);
+		public void applyToMatrix(int[][] matrix) {
+			for (int i = 0; i < h; i++) {
+				for (int j = 0; j < w; j++) {
+					matrix[topLeftPos.row + i][topLeftPos.col + j] = this.pk;
+				}
+			}
 		}
 		
+		public List<Pos> getSides(int directionIndex) {
+			List<Pos> sideList = new ArrayList<>();
+			
+			if (directionIndex == 0) {
+				for (int j = 0; j < w; j++) {
+					sideList.add(new Pos(topLeftPos.row - 1, topLeftPos.col + j));
+				}
+			}
+			
+			if (directionIndex == 1) {
+				for (int i = 0; i < h; i++) {
+					sideList.add(topLeftPos.addPos(new Pos(i, this.w)));
+				}
+			}
+			
+			if (directionIndex == 2) {
+				for (int j = 0; j < w; j++) {
+					sideList.add(topLeftPos.addPos(new Pos(this.h, j)));
+				}
+			}
+			
+			if (directionIndex == 3) {
+				for (int i = 0; i < h; i++) {
+					sideList.add(topLeftPos.addPos(new Pos(i, -1)));
+				}
+			}
+			
+			return sideList;
+		}
+		
+		public int getDamage() {
+			int damage = 0;
+			for (int i = 0; i < h; i++) {
+				for (int j = 0; j < w; j++) {
+					if (mainMatrix[this.topLeftPos.row + i][this.topLeftPos.col + j] == 1) {
+						damage += 1;
+					}
+				}
+			}
+			
+			return damage;
+		}
 	}
 	
 	public static void main(String[] args) throws Exception {
@@ -111,187 +106,136 @@ public class Main {
 	}
 	
 	public static void solution() {
+		for (int i = 0; i < Q; i++) {
+			attack(commands[i]);
+		}
 		
-		for (int turn = 0; turn < M; turn++) {
-			List<Integer> uniqueNumList = new ArrayList<>(santaMap.keySet());
-			if (uniqueNumList.size() == 0) { break; }
-	
+		
+		int answer = 0;
+		for (int i = 0; i < N; i++) {
+			int pk = i + 1;
+			if (soldierMap.get(pk) == null) { continue; }
 			
-			Pos deerMovedPos = getDeerMovedPos();
-			Pos direction = deerMovedPos.minusPos(deerPos);
-			deerPos = deerMovedPos;
-			if (mainMatrix[deerMovedPos.row][deerMovedPos.col] != 0) {
-				attack(deerMovedPos, C, direction, 2);
+			answer += initPList[i] - soldierMap.get(pk).p;
+		}
+		
+		System.out.println(answer);
+		
+	}
+	
+	public static void attack(int[] command) {
+		int attackPk = command[0];
+		int directionIndex = command[1];
+		
+		int[][] updatedSoliderMatrix = new int[L][L];
+		int[] attackedList = new int[N];
+	
+		Deque<Integer> queue = new ArrayDeque<>();
+		queue.add(attackPk);
+		
+		if (soldierMap.get(attackPk) == null) { return; }
+		
+		while (!queue.isEmpty()) {
+			int pk = queue.pollFirst();
+			attackedList[pk - 1] = 1;
+			
+			Soldier soldier = soldierMap.get(pk);
+			List<Pos> sidePosList = soldier.getSides(directionIndex);
+			
+			for (Pos pos : sidePosList) {
+				if (!pos.isValidIndex()) { return; }
+				if (mainMatrix[pos.row][pos.col] == 2) { return; }
+				if (soldierMatrix[pos.row][pos.col] == 0) { continue; }
+				if (attackedList[soldierMatrix[pos.row][pos.col] - 1] != 0) { continue; }
+				
+				queue.addLast(soldierMatrix[pos.row][pos.col]);		
+			}
+		}
+		
+		
+		for (int i = 0; i < N; i++) {
+			if (soldierMap.get(i + 1) == null) { continue; }
+			
+			if (i + 1 == attackPk) {
+				Soldier attackSoldier = soldierMap.get(attackPk);
+				attackSoldier.topLeftPos = attackSoldier.topLeftPos.addPos(directions[directionIndex]);
+				
+				soldierMap.put(attackPk, attackSoldier);
+				attackSoldier.applyToMatrix(updatedSoliderMatrix);
 			}
 			
-			
-			for (int uniqueNum = 1; uniqueNum <= P; uniqueNum++) {
-				if (santaOutedMap.get(uniqueNum) == 1) { continue; }
-				if (santaDowned[uniqueNum - 1] != 0) {
-					santaDowned[uniqueNum - 1] -= 1;
+			else if (attackedList[i] == 1) {
+				Soldier attackedSoldier = soldierMap.get(i + 1);
+				attackedSoldier.topLeftPos = attackedSoldier.topLeftPos.addPos(directions[directionIndex]);
+				
+				int damage = attackedSoldier.getDamage();
+				attackedSoldier.p -= damage;
+				
+				if (attackedSoldier.p <= 0) {
+					soldierMap.remove(attackedSoldier.pk);
 					continue;
 				}
 				
-				Pos origPos = santaMap.get(uniqueNum).curPos;
-				Pos movedPos = santaMove(uniqueNum);
-				Pos santaDirection = movedPos.minusPos(origPos);
-				if (movedPos.equals(deerPos)) {
-					attack(origPos, D, santaDirection.reversePos(), 1);
-					continue;
-				}
-				
-				
-				updateSantaInfo(uniqueNum, movedPos);
+				soldierMap.put(attackedSoldier.pk, attackedSoldier);
+				attackedSoldier.applyToMatrix(updatedSoliderMatrix);
 			}
 			
-			
-			for (int uniqueNum : santaMap.keySet()) {
-				scoreList[uniqueNum - 1] += 1;
+			else {
+				Soldier soldier = soldierMap.get(i + 1);
+				soldier.applyToMatrix(updatedSoliderMatrix);
 			}
 		}
 		
-		
-		StringBuilder sb = new StringBuilder();
-		for (int i = 0; i < P; i++) {
-			sb.append(scoreList[i] + " ");
-		}
-		System.out.println(sb.toString().substring(0, sb.length() - 1));
-	}
-	
-	public static void dfs(Pos pos, Pos direction) {
-		int uniqueNum = mainMatrix[pos.row][pos.col];
-		Pos movedPos = pos.addPos(direction);
-		if (!movedPos.isValidIndex()) {
-			removeOutedSantaInfo(uniqueNum);
-			return;
-		}
-		
-		if (mainMatrix[movedPos.row][movedPos.col] != 0) {
-			dfs(movedPos, direction);
-		}
-		
-		updateSantaInfo(uniqueNum, movedPos);
-;	}
-	
-	public static void attack(Pos santaPos, int power, Pos direction, int numRest) {
-		int attackedSantaUniqueNum = mainMatrix[santaPos.row][santaPos.col];
-		santaDowned[attackedSantaUniqueNum - 1] = numRest;
-		Santa attackedSanta = santaMap.get(attackedSantaUniqueNum);
-		
-		scoreList[attackedSantaUniqueNum - 1] += power;
-		
-		Pos poweredDirection = direction.multiplyPos(power);
-		Pos movedPos = deerPos.addPos(poweredDirection);
-		if (!movedPos.isValidIndex()) {
-			removeOutedSantaInfo(attackedSantaUniqueNum);
-			return;
-		}
-		
-		
-		if (mainMatrix[movedPos.row][movedPos.col] != 0 &&
-				!movedPos.equals(attackedSanta.curPos)) {
-			dfs(movedPos, direction);
-		}
-		
-		updateSantaInfo(attackedSantaUniqueNum, movedPos);
-	}
-	
-	public static Pos santaMove(int uniqueNum) {
-		Pos santaPos = santaMap.get(uniqueNum).curPos;
-		
-		int minDistance = santaPos.calcDistance(deerPos);
-		Pos minPos = santaPos;
-		
-		for (Pos direction : santaDirections) {
-			Pos movedPos = santaPos.addPos(direction);
-			if (!movedPos.isValidIndex()) { continue; }
-			if (mainMatrix[movedPos.row][movedPos.col] != 0) { continue; }
-			
- 			int distance = movedPos.calcDistance(deerPos);
-			if (distance < minDistance) {
-				minDistance = distance;
-				minPos = movedPos;
-			}
-		}
-		
-		return minPos;
-	}
-	
- 	
-	public static Pos getDeerMovedPos() {
-		List<Pos> santaPosList = new ArrayList<>();
-		for (int uniqueNum : santaMap.keySet()) {
-			santaPosList.add(santaMap.get(uniqueNum).curPos);
-		}
-		Collections.sort(santaPosList);
-		Pos bestDirection = null;
-		
-		Pos closestSantaPos = santaPosList.get(0);
-		int minDistance = deerPos.calcDistance(closestSantaPos);
-		
-		for (Pos direction : deerDirections) {
-			Pos movedPos = deerPos.addPos(direction);
-			if (!movedPos.isValidIndex()) { continue; }
-			
-			int distance = movedPos.calcDistance(closestSantaPos);
-			if (distance < minDistance) {
-				minDistance = distance;
-				bestDirection = direction;
-			}
-		}
-		
-		return deerPos.addPos(bestDirection);
-	}
-	
-	public static void removeOutedSantaInfo(int uniqueNum) {
-		Santa santa = santaMap.get(uniqueNum);
-		
-		mainMatrix[santa.curPos.row][santa.curPos.col] = 0;
-		santaMap.remove(uniqueNum);
-		santaOutedMap.put(uniqueNum, 1);
-	}
-	
-	public static void updateSantaInfo(int uniqueNum, Pos movedPos) {
-		Pos origPos = santaMap.get(uniqueNum).curPos;
-		
-		mainMatrix[origPos.row][origPos.col] = 0;
-		mainMatrix[movedPos.row][movedPos.col] = uniqueNum;
-		santaMap.put(uniqueNum, new Santa(uniqueNum, movedPos));
+		soldierMatrix = updatedSoliderMatrix;
 	}
 	
 	public static void init() throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		
 		StringTokenizer st = new StringTokenizer(br.readLine());
+		
+		L = Integer.parseInt(st.nextToken());
 		N = Integer.parseInt(st.nextToken());
-		M = Integer.parseInt(st.nextToken());
-		P = Integer.parseInt(st.nextToken());
-		C = Integer.parseInt(st.nextToken());
-		D = Integer.parseInt(st.nextToken());
+		Q = Integer.parseInt(st.nextToken());
 		
-		mainMatrix = new int[N][N];
-		
-		st = new StringTokenizer(br.readLine());
-		int deerRow = Integer.parseInt(st.nextToken()) - 1;
-		int deerCol = Integer.parseInt(st.nextToken()) - 1;
-		deerPos = new Pos(deerRow, deerCol);
-		
-		santaMap = new HashMap<>();
-		santaOutedMap = new HashMap<>();
-		santaDowned = new int[P];
-		for (int i = 0; i < P; i++) {
+		mainMatrix = new int[L][L];
+		soldierMatrix = new int[L][L];
+		for (int i = 0; i < L; i++) {
 			st = new StringTokenizer(br.readLine());
 			
-			int uniqueNum = Integer.parseInt(st.nextToken());
-			int santaRow = Integer.parseInt(st.nextToken()) - 1;
-			int santaCol = Integer.parseInt(st.nextToken()) - 1;
-			
-			mainMatrix[santaRow][santaCol] = uniqueNum;
-			santaMap.put(uniqueNum, new Santa(uniqueNum, new Pos(santaRow, santaCol)));
-			santaOutedMap.put(uniqueNum, 0);
+			for (int j = 0; j < L; j++) {
+				mainMatrix[i][j] = Integer.parseInt(st.nextToken());
+			}
 		}
 		
-		scoreList = new int[P];
+		
+		soldierMap = new HashMap<>();
+		initPList = new int[N];
+		for (int i = 0; i < N; i++) {
+			st = new StringTokenizer(br.readLine());
+			
+			int row = Integer.parseInt(st.nextToken()) - 1;
+			int col = Integer.parseInt(st.nextToken()) - 1;
+			int h = Integer.parseInt(st.nextToken());
+			int w = Integer.parseInt(st.nextToken());
+			int p = Integer.parseInt(st.nextToken());
+			
+			Soldier soldier = new Soldier(i + 1, new Pos(row, col), h, w, p);
+			soldierMap.put(i + 1, soldier);
+			soldier.applyToMatrix(soldierMatrix);
+			initPList[i] = p;
+		}
+		
+		
+		commands = new int[Q][2];
+		for (int i = 0; i < Q; i++) {
+			st = new StringTokenizer(br.readLine());
+			
+			int pk = Integer.parseInt(st.nextToken());
+			int directionIndex = Integer.parseInt(st.nextToken());
+			
+			commands[i][0] = pk;
+			commands[i][1] = directionIndex;
+		}
 	}
 
 }
--- src/main/java/problem/왕실의기사대결.java
+++ src/main/java/problem/왕실의기사대결.java
@@ -3,269 +3,239 @@ package problem;
 import java.util.*;
 import java.io.*;
 
-
 public class 왕실의기사대결 {
-    static int L, N, Q;
-    static int[][] chessMatrix, warriorMatrix;
-    static Map<Integer, Warrior> warriorMap;
-    static Command[] commandList;
-    static Pos[] directions = {new Pos(-1, 0), new Pos(0, 1), new Pos(1, 0), new Pos(0, - 1)};
-    
-    public static class Pos {
-    	int row;
-    	int col;
-    	
-    	public Pos(int row, int col) {
-    		this.row = row;
-    		this.col = col;
-    	}
-    	
-    	public Pos addPos(Pos direction) {
-    		return new Pos(this.row + direction.row, this.col + direction.col);
-    	}
-    	
-    	public boolean isValidIndex() {
-    		if (this.row < 0 || this.row >= L || this.col < 0 || this.col >= L) {
-    			return false;
-    		}
-    		
-    		return true;
-    	}
-    }
-    
-    public static class Warrior {
-    	int uniqueNum;
-    	Pos topLeftPos;
-    	int height;
-    	int width;
-    	int hp;
-    	int originalHP;
-    	
-    	public Warrior(int uniqueNum, Pos topLeftPos, int height, int width, int hp) {
-    		this.uniqueNum = uniqueNum;
-    		this.topLeftPos = topLeftPos;
-    		this.height = height;
-    		this.width = width;
-    		this.originalHP = hp;
-    		this.hp = hp;
-    	}
-    	
-    	public int[][] applyToMatrix(int[][] matrix) {
-    		for (int i = 0; i < height; i++) {
-    			for (int j = 0; j < width; j++) {
-    				matrix[this.topLeftPos.row + i][this.topLeftPos.col + j] = this.uniqueNum;
-    			}
-    		}
-    		
-    		return matrix;
-    	}
-    	
-    	public List<Pos> getSurroundings(int directionIndex) {
-    		if (directionIndex == 0) {
-    			return getNorthSurroundings();
-    		}
-    		
-    		if (directionIndex == 1) {
-    			return getEastSurroundings();
-    		}
-    		
-    		if (directionIndex == 2) {
-    			return getSouthSurroundings();
-    		}
-    		
-    		return getWestSurroundings();
-    	}
-    	
-    	public List<Pos> getNorthSurroundings() {
-    		List<Pos> northSurroundings = new ArrayList<>();
-    		for (int i = 0; i < width; i++) {
-    			northSurroundings.add(new Pos(this.topLeftPos.row - 1, this.topLeftPos.col + i));
-    		}
-    		
-    		return northSurroundings;
-    	}
-    	
-    	public List<Pos> getEastSurroundings() {
-    		List<Pos> eastSurroundings = new ArrayList<>();
-    		for (int i = 0; i < height; i++) {
-    			eastSurroundings.add(new Pos(this.topLeftPos.row + i, this.topLeftPos.col + this.width));
-    		}
-    		
-    		return eastSurroundings;
-    	}
-    	
-    	public List<Pos> getSouthSurroundings() {
-    		List<Pos> southSurroundings = new ArrayList<>();
-    		for (int i = 0; i < this.width; i++) {
-    			southSurroundings.add(this.topLeftPos.addPos(new Pos(this.height, i)));
-    		}
-    		
-    		return southSurroundings;
-    	}
-    	
-    	public List<Pos> getWestSurroundings() {
-    		List<Pos> westSurroundings = new ArrayList<>();
-    		for (int i = 0; i < this.height; i++) {
-    			westSurroundings.add(this.topLeftPos.addPos(new Pos(i, -1)));
-    		}
-    		
-    		return westSurroundings;
-    	}
-    	
-    	public int countDamage() {
-    		int damage = 0;
-    		for (int i = 0; i < this.height; i++) {
-    			for (int j = 0; j < this.width; j++) {
-    				if (chessMatrix[this.topLeftPos.row + i][this.topLeftPos.col + j] == 1) {
-    					damage += 1;
-    				}
-    			}
-    		}
-    		
-    		return damage;
-    	}
-    	
-    	public boolean isPosIncluded(Pos pos) {
-    		if (pos.row >= this.topLeftPos.row && pos.row < this.topLeftPos.row + this.height 
-    				&& pos.col >= this.topLeftPos.col && pos.col < this.topLeftPos.col + this.width) {
-    			return true;
-    		}
-    		
-    		return false;
-    	}
-    	
-    	public boolean isAtLeastOnePosIncluded(List<Pos> posList) {
-    		for (Pos pos : posList) {
-    			if (this.isPosIncluded(pos)) {
-    				return true;
-    			}
-    		}
-    		
-    		return false;
-    	}
-    	
-    }
-    
-    public static class Command {
-    	int uniqueNum;
-    	int directionIndex;
-    	
-    	public Command(int uniqueNum, int directionIndex) {
-    		this.uniqueNum = uniqueNum;
-    		this.directionIndex = directionIndex;
-    	}
-    }
-    
-    
-    public static void main(String[] args) throws Exception {
-    	init();
-    	
-    	
-    	for (Command command : commandList) {
-    		int flag = 0;
-    		Map<Integer, Integer> movedMap = new HashMap<>();
-    		Deque<Pos> queue = new ArrayDeque<>();
-    		
-    		movedMap.put(command.uniqueNum, 1);
-    		Warrior attackWarrior = warriorMap.get(command.uniqueNum);
-    		if (attackWarrior.hp <= 0) { continue; }
-    		queue.addAll(attackWarrior.getSurroundings(command.directionIndex));
-    		while (!queue.isEmpty()) {
-    			Pos curPos = queue.pollFirst();
-    			if (!curPos.isValidIndex() || chessMatrix[curPos.row][curPos.col] == 2) {
-    				flag = 1;
-    				break;
-    			}
-    			
-    			if (warriorMatrix[curPos.row][curPos.col] == 0) { continue; }
-    			
-    			Warrior attackedWarrior = warriorMap.get(warriorMatrix[curPos.row][curPos.col]);
-    			if (movedMap.get(attackedWarrior.uniqueNum) != null) { continue; }
-    			movedMap.put(attackedWarrior.uniqueNum, 1);
-    			
-    			queue.addAll(attackedWarrior.getSurroundings(command.directionIndex));
-    		}
-    		
-    		
-    		
-    		if (flag == 1) { continue; }
-    		
-    		int[][] updatedWarriorMatrix = new int[L][L];
-    		for (int i = 0; i < N; i++) {
-    			int uniqueNum = i + 1;
-    			Warrior warrior = warriorMap.get(uniqueNum);
-    			
-    			if (movedMap.get(uniqueNum) != null) {
-    				warrior.topLeftPos = warrior.topLeftPos.addPos(directions[command.directionIndex]);
-    				if (uniqueNum != command.uniqueNum) {
-    					int damage = warrior.countDamage();
-    					warrior.hp -= damage;
-    				}
-    			}
-    			
-    			if (warrior.hp > 0) {
-    				warrior.applyToMatrix(updatedWarriorMatrix);
-    			}
-    		}
-    		
-    		warriorMatrix = updatedWarriorMatrix;
-    	}
-    	
-    	
-    	int answer = 0;
-    	for (int i = 0; i < N; i++) {
-    		int uniqueNum = i + 1;
-    		Warrior warrior = warriorMap.get(uniqueNum);
-    		if (warrior.hp <= 0) { continue; }
-    		
-    		answer += (warrior.originalHP - warrior.hp);
-    	}
-    	
-    	System.out.println(answer);
-    }
-    
-    public static void init() throws IOException {
-    	BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-    	StringTokenizer st = new StringTokenizer(br.readLine());
-    	
-    	L = Integer.parseInt(st.nextToken());
-    	N = Integer.parseInt(st.nextToken());
-    	Q = Integer.parseInt(st.nextToken());
-    	
-    	chessMatrix = new int[L][L];
-    	warriorMatrix = new int[L][L];
-    	
-    	for (int i = 0; i < L; i++) {
-    		st = new StringTokenizer(br.readLine());
-    		for (int j = 0; j < L; j++) {
-    			chessMatrix[i][j] = Integer.parseInt(st.nextToken());
-    			
-    		}
-    	}
-    	
-    	warriorMap = new HashMap<>();
-    	for (int i = 0; i < N; i++) {
-    		st = new StringTokenizer(br.readLine());
-    		
-    		int row = Integer.parseInt(st.nextToken()) - 1;
-    		int col = Integer.parseInt(st.nextToken()) - 1;
-    		int height = Integer.parseInt(st.nextToken());
-    		int width = Integer.parseInt(st.nextToken());
-    		int hp = Integer.parseInt(st.nextToken());
-    		
-    		Warrior warrior = new Warrior(i + 1, new Pos(row, col), height, width, hp);
-    		warrior.applyToMatrix(warriorMatrix);
-    		warriorMap.put(i + 1, warrior);
-    	}
-    	
-    	commandList = new Command[Q];
-    	for (int i = 0; i < Q; i++) {
-    		st = new StringTokenizer(br.readLine());
-    		int uniqueNum = Integer.parseInt(st.nextToken());
-    		int directionIndex = Integer.parseInt(st.nextToken());
-    		
-    		commandList[i] = new Command(uniqueNum, directionIndex);
-    	}
-    	
-    }
+	static int L, N, Q;
+	static int[][] mainMatrix, soldierMatrix;
+	static Pos[] directions = {new Pos(-1, 0), new Pos(0, 1), new Pos(1, 0), new Pos(0, -1)};
+	static Map<Integer, Soldier> soldierMap;
+	static int[][] commands;
+	static int[] initPList;
+
+	public static class Pos {
+		int row;
+		int col;
+
+		public Pos(int row, int col) {
+			this.row = row;
+			this.col = col;
+		}
+
+		public Pos addPos(Pos direction) {
+			return new Pos(this.row + direction.row, this.col + direction.col);
+		}
+
+		public boolean isValidIndex() {
+			if (this.row < 0 || this.row >= L || this.col < 0 || this.col >= L) {
+				return false;
+			}
+
+			return true;
+		}
+	}
+
+	public static class Soldier {
+		int pk;
+		Pos topLeftPos;
+		int h;
+		int w;
+		int p;
+
+		public Soldier(int pk, Pos topLeftPos, int h, int w, int p) {
+			this.pk = pk;
+			this.topLeftPos = topLeftPos;
+			this.h = h;
+			this.w = w;
+			this.p = p;
+		}
+
+		public void applyToMatrix(int[][] matrix) {
+			for (int i = 0; i < h; i++) {
+				for (int j = 0; j < w; j++) {
+					matrix[topLeftPos.row + i][topLeftPos.col + j] = this.pk;
+				}
+			}
+		}
+
+		public List<Pos> getSides(int directionIndex) {
+			List<Pos> sideList = new ArrayList<>();
+
+			if (directionIndex == 0) {
+				for (int j = 0; j < w; j++) {
+					sideList.add(new Pos(topLeftPos.row - 1, topLeftPos.col + j));
+				}
+			}
+
+			if (directionIndex == 1) {
+				for (int i = 0; i < h; i++) {
+					sideList.add(topLeftPos.addPos(new Pos(i, this.w)));
+				}
+			}
+
+			if (directionIndex == 2) {
+				for (int j = 0; j < w; j++) {
+					sideList.add(topLeftPos.addPos(new Pos(this.h, j)));
+				}
+			}
+
+			if (directionIndex == 3) {
+				for (int i = 0; i < h; i++) {
+					sideList.add(topLeftPos.addPos(new Pos(i, -1)));
+				}
+			}
+
+			return sideList;
+		}
+
+		public int getDamage() {
+			int damage = 0;
+			for (int i = 0; i < h; i++) {
+				for (int j = 0; j < w; j++) {
+					if (mainMatrix[this.topLeftPos.row + i][this.topLeftPos.col + j] == 1) {
+						damage += 1;
+					}
+				}
+			}
+
+			return damage;
+		}
+	}
+
+	public static void main(String[] args) throws Exception {
+		init();
+		solution();
+	}
+
+	public static void solution() {
+		for (int i = 0; i < Q; i++) {
+			attack(commands[i]);
+		}
+
+
+		int answer = 0;
+		for (int i = 0; i < N; i++) {
+			int pk = i + 1;
+			if (soldierMap.get(pk) == null) { continue; }
+
+			answer += initPList[i] - soldierMap.get(pk).p;
+		}
+
+		System.out.println(answer);
+
+	}
+
+	public static void attack(int[] command) {
+		int attackPk = command[0];
+		int directionIndex = command[1];
+
+		int[][] updatedSoliderMatrix = new int[L][L];
+		int[] attackedList = new int[N];
+
+		Deque<Integer> queue = new ArrayDeque<>();
+		queue.add(attackPk);
+
+		if (soldierMap.get(attackPk) == null) { return; }
+
+		while (!queue.isEmpty()) {
+			int pk = queue.pollFirst();
+			attackedList[pk - 1] = 1;
+
+			Soldier soldier = soldierMap.get(pk);
+			List<Pos> sidePosList = soldier.getSides(directionIndex);
+
+			for (Pos pos : sidePosList) {
+				if (!pos.isValidIndex()) { return; }
+				if (mainMatrix[pos.row][pos.col] == 2) { return; }
+				if (soldierMatrix[pos.row][pos.col] == 0) { continue; }
+				if (attackedList[soldierMatrix[pos.row][pos.col] - 1] != 0) { continue; }
+
+				queue.addLast(soldierMatrix[pos.row][pos.col]);
+			}
+		}
+
+
+		for (int i = 0; i < N; i++) {
+			if (soldierMap.get(i + 1) == null) { continue; }
+
+			if (i + 1 == attackPk) {
+				Soldier attackSoldier = soldierMap.get(attackPk);
+				attackSoldier.topLeftPos = attackSoldier.topLeftPos.addPos(directions[directionIndex]);
+
+				soldierMap.put(attackPk, attackSoldier);
+				attackSoldier.applyToMatrix(updatedSoliderMatrix);
+			}
+
+			else if (attackedList[i] == 1) {
+				Soldier attackedSoldier = soldierMap.get(i + 1);
+				attackedSoldier.topLeftPos = attackedSoldier.topLeftPos.addPos(directions[directionIndex]);
+
+				int damage = attackedSoldier.getDamage();
+				attackedSoldier.p -= damage;
+
+				if (attackedSoldier.p <= 0) {
+					soldierMap.remove(attackedSoldier.pk);
+					continue;
+				}
+
+				soldierMap.put(attackedSoldier.pk, attackedSoldier);
+				attackedSoldier.applyToMatrix(updatedSoliderMatrix);
+			}
+
+			else {
+				Soldier soldier = soldierMap.get(i + 1);
+				soldier.applyToMatrix(updatedSoliderMatrix);
+			}
+		}
+
+		soldierMatrix = updatedSoliderMatrix;
+	}
+
+	public static void init() throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st = new StringTokenizer(br.readLine());
+
+		L = Integer.parseInt(st.nextToken());
+		N = Integer.parseInt(st.nextToken());
+		Q = Integer.parseInt(st.nextToken());
+
+		mainMatrix = new int[L][L];
+		soldierMatrix = new int[L][L];
+		for (int i = 0; i < L; i++) {
+			st = new StringTokenizer(br.readLine());
+
+			for (int j = 0; j < L; j++) {
+				mainMatrix[i][j] = Integer.parseInt(st.nextToken());
+			}
+		}
+
+
+		soldierMap = new HashMap<>();
+		initPList = new int[N];
+		for (int i = 0; i < N; i++) {
+			st = new StringTokenizer(br.readLine());
+
+			int row = Integer.parseInt(st.nextToken()) - 1;
+			int col = Integer.parseInt(st.nextToken()) - 1;
+			int h = Integer.parseInt(st.nextToken());
+			int w = Integer.parseInt(st.nextToken());
+			int p = Integer.parseInt(st.nextToken());
+
+			Soldier soldier = new Soldier(i + 1, new Pos(row, col), h, w, p);
+			soldierMap.put(i + 1, soldier);
+			soldier.applyToMatrix(soldierMatrix);
+			initPList[i] = p;
+		}
+
+
+		commands = new int[Q][2];
+		for (int i = 0; i < Q; i++) {
+			st = new StringTokenizer(br.readLine());
+
+			int pk = Integer.parseInt(st.nextToken());
+			int directionIndex = Integer.parseInt(st.nextToken());
+
+			commands[i][0] = pk;
+			commands[i][1] = directionIndex;
+		}
+	}
+
 }
