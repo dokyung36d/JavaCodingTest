aebc95be4f7620543bb3f22f57a8d13828dd5936
dokyu <dokyung36@naver.com>
2025-12-09 22:48:01 +0900

    두 배열의 합 solveD

--- src/main/java/problem/Main.java
+++ src/main/java/problem/Main.java
@@ -4,8 +4,11 @@ import java.util.*;
 import java.io.*;
 
 public class Main {
-	static int N;
-	static List<Integer> primeNumList, primeCumulativeSumList;
+	static int T;
+	static int N, M;
+	static int[] nList, mList;
+	static long[] nSumList, mSumList;
+	static Map<Long, Integer> nSumMap, mSumMap;
 
 	public static void main(String[] args) throws Exception {
 		init();
@@ -13,65 +16,67 @@ public class Main {
 	}
 
 	public static void solution() {
-		setPrimeNumList();
+		long answer = 0;
 
-		int leftIndex = 0;
-		int rightIndex = 0;
+		for (long nSum : nSumMap.keySet()) {
+			long restNum = T - nSum;
 
-		int answer = 0;
-		while (leftIndex <= rightIndex && rightIndex < primeCumulativeSumList.size()) {
-			int sumValue = primeCumulativeSumList.get(rightIndex) - primeCumulativeSumList.get(leftIndex);
+			if (mSumMap.get(restNum) == null) { continue; }
 
-			if (sumValue == N) {
-				answer += 1;
-				leftIndex += 1;
-				continue;
-			}
-
-			if (sumValue < N) {
-				rightIndex += 1;
-				continue;
-			}
-
-			if (sumValue > N) {
-				leftIndex += 1;
-				continue;
-			}
+			answer += (long) nSumMap.get(nSum) * mSumMap.get(restNum);
 		}
+
 		System.out.println(answer);
-	}
-
-	public static void setPrimeNumList() {
-		primeNumList = new ArrayList<>();
-		primeCumulativeSumList = new ArrayList<>();
-		int[] isPrime = new int[N + 1]; // 0 ~ N
-
-		Arrays.fill(isPrime, 1);
-		isPrime[0] = 0;
-		isPrime[1] = 0;
-
-		for (int i = 0; i < N + 1; i++) {
-			if (isPrime[i] == 0) { continue; }
-
-			for (int j = 2 * i; j < N + 1; j += i) {
-				isPrime[j] = 0;
-			}
-		}
-
-		int primeCumulativeSum = 0;
-		primeCumulativeSumList.add(primeCumulativeSum);
-		for (int i = 0; i < N + 1; i++) {
-			if (isPrime[i] == 0 ) { continue; }
-			primeNumList.add(i);
-			primeCumulativeSumList.add(primeCumulativeSum + i);
-			primeCumulativeSum += i;
-		}
 	}
 
 	public static void init() throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		StringTokenizer st = new StringTokenizer(br.readLine());
+		T = Integer.parseInt(st.nextToken());
 
+		st = new StringTokenizer(br.readLine());
 		N = Integer.parseInt(st.nextToken());
+
+		nList = new int[N];
+		st = new StringTokenizer(br.readLine());
+		for (int i = 0; i < N; i++) {
+			nList[i] = Integer.parseInt(st.nextToken());
+		}
+
+		nSumList = new long[N + 1];
+		for (int i = 0; i < N; i++) {
+			nSumList[i + 1] = (long) nSumList[i] + nList[i];
+		}
+
+
+		nSumMap = new HashMap<>();
+		for (int i = 0; i < N; i++) {
+			for (int j = i + 1; j < N + 1; j++) {
+				long sumValue = nSumList[j] - nSumList[i];
+				nSumMap.put(sumValue, nSumMap.getOrDefault(sumValue, 0) + 1);
+			}
+		}
+
+		st = new StringTokenizer(br.readLine());
+		M = Integer.parseInt(st.nextToken());
+
+		mList = new int[M];
+		st = new StringTokenizer(br.readLine());
+		for (int i = 0; i < M; i++) {
+			mList[i] = Integer.parseInt(st.nextToken());
+		}
+
+		mSumList = new long[M + 1];
+		for (int i = 0; i < M; i++) {
+			mSumList[i + 1] = (long) mSumList[i] + mList[i];
+		}
+
+		mSumMap = new HashMap<>();
+		for (int i = 0; i < M; i++) {
+			for (int j = i + 1; j < M + 1; j++) {
+				long sumValue = mSumList[j] - mSumList[i];
+				mSumMap.put(sumValue, mSumMap.getOrDefault(sumValue, 0) + 1);
+			}
+		}
 	}
 }
--- src/main/java/problem/두배열의합.java
+++ src/main/java/problem/두배열의합.java
@@ -1,17 +1,14 @@
 package problem;
 
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.util.HashMap;
-import java.util.Map;
-import java.util.StringTokenizer;
+import java.util.*;
+import java.io.*;
 
 public class 두배열의합 {
-    static int T, aN, bN;
-    static int[] aNList, bNList;
-    static int[] aNCumulativeSum, bNCumulativeSum;
-    static Map<Integer, Integer> aNSumMap, bNSumMap;
+    static int T;
+    static int N, M;
+    static int[] nList, mList;
+    static long[] nSumList, mSumList;
+    static Map<Long, Integer> nSumMap, mSumMap;
 
     public static void main(String[] args) throws Exception {
         init();
@@ -19,74 +16,67 @@ public class 두배열의합 {
     }
 
     public static void solution() {
-        aNCumulativeSum = setCumulativeSum(aNList);
-        bNCumulativeSum = setCumulativeSum(bNList);
-
-        aNSumMap = setSumMap(aNCumulativeSum);
-        bNSumMap = setSumMap(bNCumulativeSum);
-
-
         long answer = 0;
-        for (int aNSumValue : aNSumMap.keySet()) {
-            int restSum = T - aNSumValue;
 
-            answer += ((long) aNSumMap.get(aNSumValue)) * ((long) bNSumMap.getOrDefault(restSum, 0));
+        for (long nSum : nSumMap.keySet()) {
+            long restNum = T - nSum;
+
+            if (mSumMap.get(restNum) == null) { continue; }
+
+            answer += (long) nSumMap.get(nSum) * mSumMap.get(restNum);
         }
-
 
         System.out.println(answer);
-    }
-
-    public static Map<Integer, Integer> setSumMap(int[] cumulativeSum) {
-        Map<Integer, Integer> sumMap = new HashMap<>();
-
-        for (int gap = 1; gap < cumulativeSum.length; gap++) {
-            for (int start = 0; start + gap < cumulativeSum.length; start++) {
-                int sumValue = cumulativeSum[start + gap] - cumulativeSum[start];
-                sumMap.put(sumValue, sumMap.getOrDefault(sumValue, 0) + 1);
-            }
-        }
-
-        return sumMap;
-    }
-
-    public static int[] setCumulativeSum(int[] numList) {
-        int[] cumulativeSum = new int[numList.length + 1];
-        cumulativeSum[0] = 0;
-
-        for (int i = 1; i < numList.length + 1; i++) {
-            cumulativeSum[i] = cumulativeSum[i - 1] + numList[i - 1];
-        }
-
-        return cumulativeSum;
     }
 
     public static void init() throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine());
-
         T = Integer.parseInt(st.nextToken());
 
         st = new StringTokenizer(br.readLine());
-        aN = Integer.parseInt(st.nextToken());
+        N = Integer.parseInt(st.nextToken());
 
+        nList = new int[N];
         st = new StringTokenizer(br.readLine());
-        aNList = new int[aN];
-        for (int i = 0; i < aN; i++) {
-            aNList[i] = Integer.parseInt(st.nextToken());
+        for (int i = 0; i < N; i++) {
+            nList[i] = Integer.parseInt(st.nextToken());
         }
 
-        st = new StringTokenizer(br.readLine());
-        bN = Integer.parseInt(st.nextToken());
-
-        st = new StringTokenizer(br.readLine());
-        bNList = new int[bN];
-        for (int i = 0; i < bN; i++) {
-            bNList[i] = Integer.parseInt(st.nextToken());
+        nSumList = new long[N + 1];
+        for (int i = 0; i < N; i++) {
+            nSumList[i + 1] = (long) nSumList[i] + nList[i];
         }
 
 
+        nSumMap = new HashMap<>();
+        for (int i = 0; i < N; i++) {
+            for (int j = i + 1; j < N + 1; j++) {
+                long sumValue = nSumList[j] - nSumList[i];
+                nSumMap.put(sumValue, nSumMap.getOrDefault(sumValue, 0) + 1);
+            }
+        }
+
+        st = new StringTokenizer(br.readLine());
+        M = Integer.parseInt(st.nextToken());
+
+        mList = new int[M];
+        st = new StringTokenizer(br.readLine());
+        for (int i = 0; i < M; i++) {
+            mList[i] = Integer.parseInt(st.nextToken());
+        }
+
+        mSumList = new long[M + 1];
+        for (int i = 0; i < M; i++) {
+            mSumList[i + 1] = (long) mSumList[i] + mList[i];
+        }
+
+        mSumMap = new HashMap<>();
+        for (int i = 0; i < M; i++) {
+            for (int j = i + 1; j < M + 1; j++) {
+                long sumValue = mSumList[j] - mSumList[i];
+                mSumMap.put(sumValue, mSumMap.getOrDefault(sumValue, 0) + 1);
+            }
+        }
     }
-
-
 }
