277fbcaa93881cf1be90e5608870e569149db439
dokyu <dokyung36@naver.com>
2025-12-11 22:16:22 +0900

    음악 프로그램 solved

--- src/main/java/problem/Main.java
+++ src/main/java/problem/Main.java
@@ -4,8 +4,34 @@ import java.util.*;
 import java.io.*;
 
 public class Main {
-	static int N;
-	static int[] numList;
+	static int N, M;
+	static Map<Node, Integer> graphMap;
+	static int[] numPointedList;
+
+	public static class Node {
+		int from;
+		int to;
+
+		public Node(int from, int to) {
+			this.from = from;
+			this.to = to;
+		}
+
+		@Override
+		public int hashCode() {
+			return Objects.hash(this.from, this.to);
+		}
+
+		@Override
+		public boolean equals(Object obj) {
+			if (this == obj) { return true; }
+			if (obj == null || this.getClass() != obj.getClass()) { return false; }
+
+			Node anotherNode = (Node) obj;
+			if (this.from == anotherNode.from && this.to == anotherNode.to) { return true; }
+			return false;
+		}
+	}
 
 	public static void main(String[] args) throws Exception {
 		init();
@@ -13,33 +39,43 @@ public class Main {
 	}
 
 	public static void solution() {
-		long minSum = Long.MAX_VALUE / 2;
-		int[] answerNumList = new int[3];
+		StringBuilder sb = new StringBuilder();
+		int numPolledSinger = 0;
+		Queue<Integer> queue = new ArrayDeque<>();
+		int[] polled = new int[N];
 
-		for (int i = 0; i < N - 2; i++) {
-			int leftIndex = i + 1;
-			int rightIndex = N - 1;
+		for (int i = 0; i < N; i++) {
+			if (numPointedList[i] != 0) { continue; }
+			queue.add(i);
+			polled[i] = 1;
+		}
 
-			while (leftIndex < rightIndex) {
-				long sumValue = (long) numList[i] + (long) numList[leftIndex] + (long) numList[rightIndex];
 
-				if (Math.abs(sumValue) < minSum) {
-					minSum = Math.abs(sumValue);
-					answerNumList[0] = numList[i];
-					answerNumList[1] = numList[leftIndex];
-					answerNumList[2] = numList[rightIndex];
-				}
+		while (!queue.isEmpty()) {
+			int singer = queue.poll();
+			numPolledSinger += 1;
 
-				if (sumValue < 0) {
-					leftIndex += 1;
-				}
-				else {
-					rightIndex -= 1;
+			sb.append(singer + 1);
+			sb.append("\n");
+
+			for (int nextSinger = 0; nextSinger < N; nextSinger++) {
+				if (polled[nextSinger] == 1) { continue; }
+				int numDirected = graphMap.getOrDefault(new Node(singer, nextSinger), 0);
+				numPointedList[nextSinger] -= numDirected;
+
+				if (numPointedList[nextSinger] == 0) {
+					queue.add(nextSinger);
+					polled[nextSinger] = 1;
 				}
 			}
 		}
 
-		System.out.println(answerNumList[0] + " " + answerNumList[1] + " " + answerNumList[2]);
+		if (numPolledSinger == N) {
+			System.out.println(sb.toString().substring(0, sb.length() - 1));
+		}
+		else {
+			System.out.println(0);
+		}
 	}
 
 	public static void init() throws IOException {
@@ -47,14 +83,30 @@ public class Main {
 		StringTokenizer st = new StringTokenizer(br.readLine());
 
 		N = Integer.parseInt(st.nextToken());
+		M = Integer.parseInt(st.nextToken());
 
-		numList = new int[N];
-		st = new StringTokenizer(br.readLine());
-		for (int i = 0; i < N; i++) {
-			numList[i] = Integer.parseInt(st.nextToken());
+		numPointedList = new int[N];
+		graphMap = new HashMap<>();
+
+		for (int i = 0; i < M; i++) {
+			st = new StringTokenizer(br.readLine());
+
+			int numSinger = Integer.parseInt(st.nextToken());
+			int[] singerList = new int[numSinger];
+
+			for (int j = 0; j < numSinger; j++) {
+				singerList[j] = Integer.parseInt(st.nextToken()) - 1;
+			}
+
+			for (int firstSingerIndex = 0; firstSingerIndex < numSinger; firstSingerIndex++) {
+				for (int secondSingerIndex = firstSingerIndex + 1; secondSingerIndex < numSinger; secondSingerIndex++) {
+					Node node = new Node(singerList[firstSingerIndex], singerList[secondSingerIndex]);
+
+					graphMap.put(node, graphMap.getOrDefault(node, 0) + 1);
+					numPointedList[singerList[secondSingerIndex]] += 1;
+				}
+			}
 		}
-
-		Arrays.sort(numList);
 	}
 
 }
--- src/main/java/problem/음악프로그램.java
+++ src/main/java/problem/음악프로그램.java
@@ -3,11 +3,35 @@ package problem;
 import java.util.*;
 import java.io.*;
 
-
 public class 음악프로그램 {
     static int N, M;
-    static Map<Integer, List<Integer>> graphMap;
-    static int[] numPointed;
+    static Map<Node, Integer> graphMap;
+    static int[] numPointedList;
+
+    public static class Node {
+        int from;
+        int to;
+
+        public Node(int from, int to) {
+            this.from = from;
+            this.to = to;
+        }
+
+        @Override
+        public int hashCode() {
+            return Objects.hash(this.from, this.to);
+        }
+
+        @Override
+        public boolean equals(Object obj) {
+            if (this == obj) { return true; }
+            if (obj == null || this.getClass() != obj.getClass()) { return false; }
+
+            Node anotherNode = (Node) obj;
+            if (this.from == anotherNode.from && this.to == anotherNode.to) { return true; }
+            return false;
+        }
+    }
 
     public static void main(String[] args) throws Exception {
         init();
@@ -15,38 +39,44 @@ public class 음악프로그램 {
     }
 
     public static void solution() {
-        PriorityQueue<Integer> pq = new PriorityQueue<>();
+        StringBuilder sb = new StringBuilder();
+        int numPolledSinger = 0;
+        Queue<Integer> queue = new ArrayDeque<>();
+        int[] polled = new int[N];
+
         for (int i = 0; i < N; i++) {
-            if (numPointed[i] == 0) {
-                pq.add(i);
-            }
+            if (numPointedList[i] != 0) { continue; }
+            queue.add(i);
+            polled[i] = 1;
         }
 
-        int numOut = 0;
 
-        StringBuilder sb = new StringBuilder();
-        while (!pq.isEmpty()) {
-            int curNum = pq.poll();
-            sb.append(curNum + 1);
+        while (!queue.isEmpty()) {
+            int singer = queue.poll();
+            numPolledSinger += 1;
+
+            sb.append(singer + 1);
             sb.append("\n");
-            numOut += 1;
 
-            for (int nearNum : graphMap.get(curNum)) {
-                numPointed[nearNum] -= 1;
+            for (int nextSinger = 0; nextSinger < N; nextSinger++) {
+                if (polled[nextSinger] == 1) { continue; }
+                int numDirected = graphMap.getOrDefault(new Node(singer, nextSinger), 0);
+                numPointedList[nextSinger] -= numDirected;
 
-                if (numPointed[nearNum] == 0) {
-                    pq.add(nearNum);
+                if (numPointedList[nextSinger] == 0) {
+                    queue.add(nextSinger);
+                    polled[nextSinger] = 1;
                 }
             }
         }
 
-        if (numOut != N) {
-            System.out.println(0);
-            return;
+        if (numPolledSinger == N) {
+            System.out.println(sb.toString().substring(0, sb.length() - 1));
         }
-        System.out.println(sb.toString().substring(0, sb.length() - 1));
+        else {
+            System.out.println(0);
+        }
     }
-
 
     public static void init() throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
@@ -55,25 +85,28 @@ public class 음악프로그램 {
         N = Integer.parseInt(st.nextToken());
         M = Integer.parseInt(st.nextToken());
 
-        numPointed = new int[N];
+        numPointedList = new int[N];
         graphMap = new HashMap<>();
-        for (int i = 0; i < N; i++) {
-            graphMap.put(i, new ArrayList<>());
-        }
 
         for (int i = 0; i < M; i++) {
             st = new StringTokenizer(br.readLine());
 
-            int numOrder = Integer.parseInt(st.nextToken());
-            int[] orderList = new int[numOrder];
-            for (int order = 0; order < numOrder; order++) {
-                orderList[order] = Integer.parseInt(st.nextToken()) - 1;
+            int numSinger = Integer.parseInt(st.nextToken());
+            int[] singerList = new int[numSinger];
+
+            for (int j = 0; j < numSinger; j++) {
+                singerList[j] = Integer.parseInt(st.nextToken()) - 1;
             }
 
-            for (int j = 0; j < numOrder - 1; j++) {
-                graphMap.get(orderList[j]).add(orderList[j + 1]);
-                numPointed[orderList[j + 1]] += 1;
+            for (int firstSingerIndex = 0; firstSingerIndex < numSinger; firstSingerIndex++) {
+                for (int secondSingerIndex = firstSingerIndex + 1; secondSingerIndex < numSinger; secondSingerIndex++) {
+                    Node node = new Node(singerList[firstSingerIndex], singerList[secondSingerIndex]);
+
+                    graphMap.put(node, graphMap.getOrDefault(node, 0) + 1);
+                    numPointedList[singerList[secondSingerIndex]] += 1;
+                }
             }
         }
     }
+
 }
