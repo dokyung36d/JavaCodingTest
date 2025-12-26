a90858486120199eb4778184a08f7ed3dadc2536
윤도경 <youndokyung@yundogyeong-ui-Macmini.local>
2025-12-26 21:39:36 +0900

    피리부는 사나이 solved

--- src/main/java/problem/Main.java
+++ src/main/java/problem/Main.java
@@ -4,47 +4,132 @@ import java.util.*;
 import java.io.*;
 
 public class Main {
-	static int N;
-	static int[][] infoList;
+	static int N, M;
+    static Pos[][] parentMatrix;
+    static Pos[] directions = {new Pos(-1, 0), new Pos(0, 1), new Pos(1, 0), new Pos(0, -1)};
+    static int[][] directionIndexMatrix;
 
-	public static void main(String[] args) throws Exception {
-		init();
-		solution();
-	}
+    public static class Pos {
+        int row;
+        int col;
 
-	public static void solution() {
-		int[][] dpMatrix = new int[N][N];
-		for (int i = 0; i < N; i++) {
-			Arrays.fill(dpMatrix[i], Integer.MAX_VALUE / 2);
-			dpMatrix[i][i] = 0;
-		}
+        public Pos(int row, int col) {
+            this.row = row;
+            this.col = col;
+        }
 
-		for (int gap = 1; gap < N; gap++) {
-			for (int startIndex = 0; startIndex + gap < N; startIndex++) {
-				int endIndex = startIndex + gap;
-				for (int midIndex = startIndex; midIndex < endIndex; midIndex++) {
-					dpMatrix[startIndex][endIndex] = Math.min(dpMatrix[startIndex][endIndex],
-							dpMatrix[startIndex][midIndex] + dpMatrix[midIndex + 1][endIndex] +
-									infoList[startIndex][0] * infoList[midIndex][1] * infoList[endIndex][1]);
-				}
-			}
-		}
+        public Pos addPos(Pos direction) {
+            return new Pos(this.row + direction.row, this.col + direction.col);
+        }
 
-		System.out.println(dpMatrix[0][N - 1]);
-	}
+        public boolean isValidIndex() {
+            if (this.row < 0 || this.row >= N || this.col < 0 || this.col >= M) {
+                return false;
+            }
 
-	public static void init() throws IOException {
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		StringTokenizer st = new StringTokenizer(br.readLine());
+            return true;
+        }
 
-		N = Integer.parseInt(st.nextToken());
+        @Override
+        public boolean equals(Object obj) {
+            if (this == obj) { return true; }
+            if (obj == null || this.getClass() != obj.getClass()) { return false; }
 
-		infoList = new int[N][2];
-		for (int i = 0; i < N; i++) {
-			st = new StringTokenizer(br.readLine());
+            Pos anotherPos = (Pos) obj;
+            if (this.row == anotherPos.row && this.col == anotherPos.col) { return true; }
+            return false;
+        }
 
-			infoList[i][0] = Integer.parseInt(st.nextToken());
-			infoList[i][1] = Integer.parseInt(st.nextToken());
-		}
-	}
+        @Override
+        public int hashCode() {
+            return Objects.hash(this.row, this.col);
+        }
+    }
+
+    public static void main(String[] args) throws Exception {
+        init();
+        solution();
+    }
+
+    public static void solution() {
+        for (int i = 0; i < N; i++) {
+            for (int j = 0; j < M; j++) {
+                Pos curPos = new Pos(i, j);
+
+                Pos direction = directions[directionIndexMatrix[curPos.row][curPos.col]];
+                Pos directedPos = curPos.addPos(direction);
+
+                union(curPos, directedPos);
+            }
+        }
+
+        int answer = 0;
+        Map<Pos, Integer> parentMap = new HashMap<>();
+        for (int i = 0; i < N; i++) {
+            for (int j = 0; j < M; j++) {
+                Pos parent = findParent(new Pos(i, j));
+
+                parentMap.put(parent, 1);
+            }
+        }
+
+        System.out.println(parentMap.keySet().size());
+    }
+
+    public static Pos findParent(Pos pos) {
+        if (pos.equals(parentMatrix[pos.row][pos.col])) { return pos; }
+
+        return parentMatrix[pos.row][pos.col] = findParent(parentMatrix[pos.row][pos.col]);
+    }
+
+    public static void union(Pos pos1, Pos pos2) {
+        Pos pos1Parent = findParent(pos1);
+        Pos pos2Parent = findParent(pos2);
+
+        if (pos1Parent.equals(pos2Parent)) { return; }
+        parentMatrix[pos1Parent.row][pos1Parent.col] = pos2Parent;
+    }
+
+    public static void init() throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        N = Integer.parseInt(st.nextToken());
+        M = Integer.parseInt(st.nextToken());
+
+        parentMatrix = new Pos[N][M];
+        for (int i = 0; i < N; i++) {
+            for (int j = 0; j < M; j++) {
+                parentMatrix[i][j] = new Pos(i, j);
+            }
+        }
+
+        directionIndexMatrix = new int[N][M];
+        for (int i = 0; i < N; i++) {
+            st = new StringTokenizer(br.readLine());
+            String string = st.nextToken();
+
+            for (int j = 0; j < M; j++) {
+                char direction  = string.charAt(j);
+
+                if (direction == 'U') {
+                    directionIndexMatrix[i][j] = 0;
+                }
+
+                else if (direction == 'R') {
+                    directionIndexMatrix[i][j] = 1;
+                }
+
+                else if (direction == 'D') {
+                    directionIndexMatrix[i][j] = 2;
+                }
+
+                else {
+                    directionIndexMatrix[i][j] = 3;
+                }
+            }
+        }
+
+
+    }
 }
--- src/main/java/problem/피리부는사나이.java
+++ src/main/java/problem/피리부는사나이.java
@@ -3,22 +3,23 @@ package problem;
 import java.util.*;
 import java.io.*;
 
-
 public class 피리부는사나이 {
     static int N, M;
-    static int[][] directionIndexMatrix;
     static Pos[][] parentMatrix;
-    static Pos[] directions = {new Pos(-1, 0), new Pos(0, 1),
-            new Pos(1, 0), new Pos(0, -1)};
+    static Pos[] directions = {new Pos(-1, 0), new Pos(0, 1), new Pos(1, 0), new Pos(0, -1)};
+    static int[][] directionIndexMatrix;
 
-
-    public static class Pos implements Comparable<Pos> {
+    public static class Pos {
         int row;
         int col;
 
-        public Pos (int row, int col) {
+        public Pos(int row, int col) {
             this.row = row;
             this.col = col;
+        }
+
+        public Pos addPos(Pos direction) {
+            return new Pos(this.row + direction.row, this.col + direction.col);
         }
 
         public boolean isValidIndex() {
@@ -29,24 +30,6 @@ public class 피리부는사나이 {
             return true;
         }
 
-        public Pos addPos(Pos direction) {
-            return new Pos(this.row + direction.row, this.col + direction.col);
-        }
-
-        @Override
-        public int compareTo(Pos anotherPos) {
-            if (this.row == anotherPos.row) {
-                return Integer.compare(this.col, anotherPos.col);
-            }
-
-            return Integer.compare(this.row, anotherPos.row);
-        }
-
-        @Override
-        public int hashCode() {
-            return Objects.hash(this.row, this.col);
-        }
-
         @Override
         public boolean equals(Object obj) {
             if (this == obj) { return true; }
@@ -54,8 +37,12 @@ public class 피리부는사나이 {
 
             Pos anotherPos = (Pos) obj;
             if (this.row == anotherPos.row && this.col == anotherPos.col) { return true; }
-
             return false;
+        }
+
+        @Override
+        public int hashCode() {
+            return Objects.hash(this.row, this.col);
         }
     }
 
@@ -65,39 +52,34 @@ public class 피리부는사나이 {
     }
 
     public static void solution() {
-        parentMatrix = new Pos[N][M];
-        for (int i = 0; i < N; i++) {
-            for (int j = 0; j < M; j++) {
-                parentMatrix[i][j] = new Pos(i, j);
-            }
-        }
-
         for (int i = 0; i < N; i++) {
             for (int j = 0; j < M; j++) {
                 Pos curPos = new Pos(i, j);
-                Pos nextPos = curPos.addPos(directions[directionIndexMatrix[i][j]]);
-                if (!nextPos.isValidIndex()) { continue; }
 
-                union(curPos, nextPos);
+                Pos direction = directions[directionIndexMatrix[curPos.row][curPos.col]];
+                Pos directedPos = curPos.addPos(direction);
+
+                union(curPos, directedPos);
             }
         }
 
+        int answer = 0;
         Map<Pos, Integer> parentMap = new HashMap<>();
         for (int i = 0; i < N; i++) {
             for (int j = 0; j < M; j++) {
-                Pos parentPos = findParent(new Pos(i, j));
-                parentMap.put(parentPos, 1);
+                Pos parent = findParent(new Pos(i, j));
+
+                parentMap.put(parent, 1);
             }
         }
 
         System.out.println(parentMap.keySet().size());
     }
 
-    public static Pos findParent(Pos curPos) {
-        if (curPos.equals(parentMatrix[curPos.row][curPos.col])) { return curPos; }
+    public static Pos findParent(Pos pos) {
+        if (pos.equals(parentMatrix[pos.row][pos.col])) { return pos; }
 
-        Pos parentPos = parentMatrix[curPos.row][curPos.col];
-        return parentMatrix[parentPos.row][parentPos.col] = findParent(parentPos);
+        return parentMatrix[pos.row][pos.col] = findParent(parentMatrix[pos.row][pos.col]);
     }
 
     public static void union(Pos pos1, Pos pos2) {
@@ -105,47 +87,49 @@ public class 피리부는사나이 {
         Pos pos2Parent = findParent(pos2);
 
         if (pos1Parent.equals(pos2Parent)) { return; }
-
-        if (pos1Parent.compareTo(pos2Parent) < 0) {
-            parentMatrix[pos1Parent.row][pos1Parent.col] = pos2Parent;
-        }
-        else {
-            parentMatrix[pos2Parent.row][pos2Parent.col] = pos1Parent;
-        }
+        parentMatrix[pos1Parent.row][pos1Parent.col] = pos2Parent;
     }
 
     public static void init() throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-
         StringTokenizer st = new StringTokenizer(br.readLine());
+
         N = Integer.parseInt(st.nextToken());
         M = Integer.parseInt(st.nextToken());
+
+        parentMatrix = new Pos[N][M];
+        for (int i = 0; i < N; i++) {
+            for (int j = 0; j < M; j++) {
+                parentMatrix[i][j] = new Pos(i, j);
+            }
+        }
 
         directionIndexMatrix = new int[N][M];
         for (int i = 0; i < N; i++) {
             st = new StringTokenizer(br.readLine());
-            String directions = st.nextToken();
+            String string = st.nextToken();
+
             for (int j = 0; j < M; j++) {
-                char direction = directions.charAt(j);
-                int directionIndex;
+                char direction  = string.charAt(j);
+
                 if (direction == 'U') {
-                    directionIndex = 0;
-                }
-                else if (direction == 'R') {
-                    directionIndex = 1;
-                }
-                else if (direction == 'D') {
-                    directionIndex = 2;
-                }
-                else {
-                    directionIndex = 3;
+                    directionIndexMatrix[i][j] = 0;
                 }
 
-                directionIndexMatrix[i][j] = directionIndex;
+                else if (direction == 'R') {
+                    directionIndexMatrix[i][j] = 1;
+                }
+
+                else if (direction == 'D') {
+                    directionIndexMatrix[i][j] = 2;
+                }
+
+                else {
+                    directionIndexMatrix[i][j] = 3;
+                }
             }
         }
+
+
     }
-
-
-
 }
