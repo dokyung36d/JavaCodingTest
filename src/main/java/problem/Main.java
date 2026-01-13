b15718d34716cdb0874b9d5b510eb10109c4842a
dokyu <dokyung36@naver.com>
2026-01-13 10:38:57 +0900

    feat: complete Union Find algorithms
    - Implemented core Union Find logic and solved related problems.
    - Next step: Solve Knapsack (Dynamic Programming) problems.

--- src/main/java/problem/Main.java
+++ src/main/java/problem/Main.java
@@ -6,33 +6,11 @@ import java.io.*;
 
 
 public class Main {
-	static int N, M;
-	static int[][] mainMatrix;
-	static int[][] dpMatrix;
-	static Map<Integer, Integer> groupSizeMap;
-	static Pos[] directions = {new Pos(-1, 0), new Pos(0, 1), new Pos(1, 0), new Pos(0, -1)};
-
-	public static class Pos {
-		int row;
-		int col;
-
-		public Pos(int row, int col) {
-			this.row = row;
-			this.col = col;
-		}
-
-		public Pos addPos(Pos direction) {
-			return new Pos(this.row + direction.row, this.col + direction.col);
-		}
-
-		public boolean isValidIndex() {
-			if (this.row < 0 || this.row >= N || this.col < 0 || this.col >= M) {
-				return false;
-			}
-
-			return true;
-		}
-	}
+	static int N, M, K;
+	static int[] numCandyList;
+	static int[][] graphMatrix;
+	static int[] parentList;
+	static Map<Integer, Integer> groupMap;
 
 	public static void main(String[] args) throws Exception {
 		init();
@@ -40,80 +18,40 @@ public class Main {
 	}
 
 	public static void solution() {
-		setDpMatrix();
-
-
-		int[][] answerMatrix = new int[N][M];
-		for (int i = 0; i < N; i++) {
-			for (int j = 0; j < M; j++) {
-				if (mainMatrix[i][j] == 0) { continue; }
-
-				Map<Integer, Integer> nearGroupMap = new HashMap<>();
-				for (Pos direction : directions) {
-					Pos movedPos = new Pos(i, j).addPos(direction);
-					if (!movedPos.isValidIndex()) { continue; }
-					if (mainMatrix[movedPos.row][movedPos.col] == 1) { continue; }
-
-					nearGroupMap.put(dpMatrix[movedPos.row][movedPos.col], 1);
-				}
-
-				int totalSize = 0;
-				for (int uniqueNum : nearGroupMap.keySet()) {
-					totalSize += groupSizeMap.get(uniqueNum);
-				}
-				totalSize += 1;
-				answerMatrix[i][j] = totalSize % 10;
-			}
+		for (int i = 0; i < M; i++) {
+			union(graphMatrix[i][0], graphMatrix[i][1]);
 		}
 
-
-		StringBuilder sb = new StringBuilder();
+		groupMap = new HashMap<>();
 		for (int i = 0; i < N; i++) {
-			for (int j = 0; j < M; j++) {
-				sb.append(answerMatrix[i][j]);
-			}
-			sb.append("\n");
+			int parent = findParent(i);
+
+			groupMap.put(parent, groupMap.getOrDefault(parent, 0) + numCandyList[i]);
 		}
-		System.out.println(sb.toString().substring(0, sb.length() - 1));
+
+		List<Integer> keyList = new ArrayList<>(groupMap.keySet());
+		int[][] dpMatrix = new int[keyList.size() + 1][K + 1];
+		for (int i = 0; i < gra)
+
+		System.out.println("hello world");
 	}
 
-	public static void setDpMatrix() {
-		dpMatrix = new int[N][M];
-		groupSizeMap = new HashMap<>();
+	public static int findParent(int num) {
+		if (num == parentList[num]) { return num; }
 
-		int[][] visited = new int[N][M];
-		int uniqueNum = 1;
-		for (int i = 0; i < N; i++) {
-			for (int j = 0; j < M; j++) {
-				if (mainMatrix[i][j] == 1) { continue; }
-				if (visited[i][j] == 1) { continue; }
+		return parentList[num] = findParent(parentList[num]);
+	}
 
-				int groupSize = 0;
+	public static void union(int num1, int num2) {
+		int num1Parent = findParent(num1);
+		int num2Parent = findParent(num2);
 
-				Deque<Pos> queue = new ArrayDeque<>();
-				queue.add(new Pos(i, j));
+		if (num1Parent == num2Parent) { return; }
 
-				while (!queue.isEmpty()) {
-					Pos curPos = queue.poll();
-					if (visited[curPos.row][curPos.col] == 1) { continue; }
-					visited[curPos.row][curPos.col] = 1;
-					dpMatrix[curPos.row][curPos.col] = uniqueNum;
-					groupSize += 1;
+		int minParent = Math.min(num1Parent, num2Parent);
+		int maxParent = Math.max(num1Parent, num2Parent);
 
-					for (Pos direction : directions) {
-						Pos movedPos = curPos.addPos(direction);
-						if (!movedPos.isValidIndex()) { continue; }
-						if (visited[movedPos.row][movedPos.col] == 1) { continue; }
-						if (mainMatrix[movedPos.row][movedPos.col] == 1) { continue; }
-
-						queue.addLast(movedPos);
-					}
-				}
-
-				groupSizeMap.put(uniqueNum, groupSize);
-				uniqueNum += 1;
-			}
-		}
+		parentList[maxParent] = minParent;
 	}
 
 	public static void init() throws IOException {
@@ -122,14 +60,25 @@ public class Main {
 
 		N = Integer.parseInt(st.nextToken());
 		M = Integer.parseInt(st.nextToken());
+		K = Integer.parseInt(st.nextToken());
 
-		mainMatrix = new int[N][M];
+		numCandyList = new int[N];
+		st = new StringTokenizer(br.readLine());
 		for (int i = 0; i < N; i++) {
+			numCandyList[i] = Integer.parseInt(st.nextToken());
+		}
+
+		graphMatrix = new int[M][2];
+		for (int i = 0; i < M; i++) {
 			st = new StringTokenizer(br.readLine());
-			String string = st.nextToken();
-			for (int j = 0; j < M; j++) {
-				mainMatrix[i][j] = Character.getNumericValue(string.charAt(j));
-			}
+
+			graphMatrix[i][0] = Integer.parseInt(st.nextToken()) - 1;
+			graphMatrix[i][1] = Integer.parseInt(st.nextToken()) - 1;
+		}
+
+		parentList = new int[N];
+		for (int i = 0; i < N; i++) {
+			parentList[i] = i;
 		}
 	}
 }
