cad2483339b72e445855e713fec2152dead1b12d
윤도경 <youndokyung@yundogyeong-ui-Macmini.local>
2026-01-31 09:40:49 +0900

    열쇠 solved

--- src/main/java/problem/Main.java
+++ src/main/java/problem/Main.java
@@ -6,81 +6,179 @@ import java.io.*;
 
 
 public class Main {
-	static int N;
-	static int[][] graphMatrix;
-	static int allVisited;
+    static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    static int N, M;
+    static char[][] mainMatrix;
+    static Map<Character, Integer> keyMap;
+    static Pos[] directions = {new Pos(-1, 0), new Pos(0, 1), new Pos(1, 0), new Pos(0, -1)};
 
-	public static class Node implements Comparable<Node> {
-		int curPos;
-		int cost;
-		int visited;
 
-		public Node(int curPos, int cost, int visited) {
-			this.curPos = curPos;
-			this.cost = cost;
-			this.visited = visited;
-		}
+    public static class Pos {
+        int row;
+        int col;
 
-		@Override
-		public int compareTo(Node anotherNode) {
-			return Integer.compare(this.cost, anotherNode.cost);
-		}
-	}
+        public Pos(int row, int col) {
+            this.row = row;
+            this.col = col;
+        }
 
-	public static void main(String[] args) throws Exception {
-		init();
-		solution();
-	}
+        public Pos addPos(Pos direction) {
+            return new Pos(this.row + direction.row, this.col + direction.col);
+        }
 
-	public static void solution() {
-		int answer = Integer.MAX_VALUE;
+        public boolean isValidIndex() {
+            if (this.row < 0 || this.row >= N || this.col < 0 || this.col >= M) {
+                return false;
+            }
 
-		int[][] visitedMatrix = new int[N][(int) Math.pow(2, N)];
+            return true;
+        }
 
-		PriorityQueue<Node> pq = new PriorityQueue<>();
-		pq.add(new Node(0, 0, 1));
+        public boolean isEdge() {
+            if (this.row == 0 || this.row == N - 1 || this.col == 0 || this.col == M - 1) {
+                return true;
+            }
 
-		while (!pq.isEmpty()) {
-			Node node = pq.poll();
-			if (visitedMatrix[node.curPos][node.visited] == 1) { continue; }
-			visitedMatrix[node.curPos][node.visited] = 1;
+            return false;
+        }
 
-			if (node.visited == allVisited) {
-				if (graphMatrix[node.curPos][0] == 0) { continue; }
+        public char getValue() {
+            return mainMatrix[this.row][this.col];
+        }
 
-				int totalCost = node.cost + graphMatrix[node.curPos][0];
-				answer = Math.min(answer, totalCost);
-			}
+        @Override
+        public boolean equals(Object obj) {
+            if (this == obj) { return true;}
+            if (obj == null || this.getClass() != obj.getClass()) { return false; }
 
-			for (int nextPos = 0; nextPos < N; nextPos++) {
-				if (graphMatrix[node.curPos][nextPos] == 0 ) { continue; }
-				if ((node.visited & (1 << nextPos)) != 0) { continue; }
-				int updatedVisited = node.visited | (1 << nextPos);
-				if (visitedMatrix[nextPos][updatedVisited] == 1) { continue; }
+            Pos anotherPos = (Pos) obj;
+            if (this.row == anotherPos.row && this.col == anotherPos.col) { return true; }
+            return false;
+        }
 
-				pq.add(new Node(nextPos, node.cost + graphMatrix[node.curPos][nextPos], updatedVisited));
-			}
-		}
+        @Override
+        public int hashCode() {
+            return Objects.hash(this.row, this.col);
+        }
+    }
 
-		System.out.println(answer);
-	}
 
-	public static void init() throws IOException {
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		StringTokenizer st = new StringTokenizer(br.readLine());
+    public static void main(String[] args) throws Exception {
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int T = Integer.parseInt(st.nextToken());
 
-		N = Integer.parseInt(st.nextToken());
-		graphMatrix = new int[N][N];
 
-		for (int i = 0; i < N; i++) {
-			st = new StringTokenizer(br.readLine());
+        for (int i = 0; i < T; i++) {
+            init();
+            solution();
+        }
+    }
 
-			for (int j = 0; j < N; j++) {
-				graphMatrix[i][j] = Integer.parseInt(st.nextToken());
-			}
-		}
+    public static void solution() {
+        int answer = 0;
+        Map<Character, List<Pos>> blockedListMap = new HashMap<>();
+        String alphabets = "qwertyuiopasdfghjklzxcvbnm";
+        for (int i = 0; i < alphabets.length(); i++) {
+            blockedListMap.put(alphabets.charAt(i), new ArrayList<>());
+        }
 
-		allVisited = (int) Math.pow(2, N) - 1;
 
-	}
+        Queue<Pos> queue = new ArrayDeque<>();
+        List<Pos> edgePosList = new ArrayList<>();
+        for (int i = 0; i < M; i++) {
+            edgePosList.add(new Pos(0, i));
+            edgePosList.add(new Pos(N - 1, i));
+        }
+
+        for (int i = 1; i < N - 1; i++) {
+            edgePosList.add(new Pos(i, 0));
+            edgePosList.add(new Pos(i, M - 1));
+        }
+
+
+        for (Pos pos : edgePosList) {
+            char value = mainMatrix[pos.row][pos.col];
+
+            if (value != '*' && !Character.isUpperCase(value)) {
+                queue.add(pos);
+                continue;
+            }
+
+            if (Character.isUpperCase(value) && keyMap.get(Character.toLowerCase(value)) == null) {
+                blockedListMap.get(Character.toLowerCase(value)).add(pos);
+            }
+
+            else if (Character.isUpperCase(value) && keyMap.get(Character.toLowerCase(value)) != null) {
+                queue.add(pos);
+            }
+        }
+
+
+        int[][] visited = new int[N][M];
+        while (!queue.isEmpty()) {
+            Pos curPos = queue.poll();
+            if (visited[curPos.row][curPos.col] == 1) { continue; }
+            visited[curPos.row][curPos.col] = 1;
+
+            if (curPos.getValue() == '$') {
+                answer += 1;
+            }
+
+            else if (Character.isLowerCase(curPos.getValue())) {
+                keyMap.put(curPos.getValue(), 1);
+                queue.addAll(blockedListMap.get(curPos.getValue()));
+
+                blockedListMap.put(curPos.getValue(), new ArrayList<>());
+            }
+
+            else if (Character.isUpperCase(curPos.getValue()) && keyMap.get(Character.toLowerCase(curPos.getValue())) == null) {
+                blockedListMap.get(Character.toLowerCase(curPos.getValue())).add(curPos);
+            }
+
+            for (Pos direction : directions) {
+                Pos movedPos = curPos.addPos(direction);
+                if (!movedPos.isValidIndex()) { continue; }
+                if (movedPos.getValue() == '*') { continue; }
+                if (visited[movedPos.row][movedPos.col] == 1) { continue; }
+
+                if (Character.isUpperCase(movedPos.getValue()) && keyMap.get(Character.toLowerCase(movedPos.getValue())) == null) {
+                    blockedListMap.get(Character.toLowerCase(movedPos.getValue())).add(movedPos);
+                    continue;
+                }
+
+                queue.add(movedPos);
+            }
+        }
+
+        System.out.println(answer);
+    }
+
+
+    public static void init() throws IOException {
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        N = Integer.parseInt(st.nextToken());
+        M = Integer.parseInt(st.nextToken());
+
+        mainMatrix = new char[N][M];
+        for (int i = 0; i < N; i++) {
+            st = new StringTokenizer(br.readLine());
+            String string = st.nextToken();
+
+            for (int j = 0; j < M; j++) {
+                mainMatrix[i][j] = string.charAt(j);
+            }
+        }
+
+        keyMap = new HashMap<>();
+        st = new StringTokenizer(br.readLine());
+        String string = st.nextToken();
+        for (int i = 0; i < string.length(); i++) {
+            char key = string.charAt(i);
+            keyMap.put(key, 1);
+        }
+
+
+
+    }
 }
--- src/main/java/problem/열쇠.java
+++ src/main/java/problem/열쇠.java
@@ -3,18 +3,14 @@ package problem;
 import java.util.*;
 import java.io.*;
 
+
+
 public class 열쇠 {
-    static int T;
-    static char[][] matrix;
     static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-    static String doorString = "QWERTYUIOPASDFGHJKLZXCVBNM";
-    static String keyString = "qwertyuiopasdfghjklzxcvbnm";
-    static Map<Character, List<Pos>> doorMap;
-    static Map<Character, List<Pos>> blockedPosMap;
-    static Pos[] directions = {new Pos(-1, 0), new Pos(1, 0), new Pos(0, -1), new Pos(0, 1)};
-    static int row, col;
-    static Deque<Pos> queue = new ArrayDeque<>();
-    static int[][] visited;
+    static int N, M;
+    static char[][] mainMatrix;
+    static Map<Character, Integer> keyMap;
+    static Pos[] directions = {new Pos(-1, 0), new Pos(0, 1), new Pos(1, 0), new Pos(0, -1)};
 
 
     public static class Pos {
@@ -26,39 +22,37 @@ public class 열쇠 {
             this.col = col;
         }
 
-        public Pos addPos(Pos anotherPos) {
-            return new Pos(this.row + anotherPos.row, this.col + anotherPos.col);
+        public Pos addPos(Pos direction) {
+            return new Pos(this.row + direction.row, this.col + direction.col);
         }
 
-        public boolean checkPosAvail() {
-            if (this.row < 0 || this.row >= matrix.length || this.col < 0 || this.col >= matrix[0].length) {
+        public boolean isValidIndex() {
+            if (this.row < 0 || this.row >= N || this.col < 0 || this.col >= M) {
                 return false;
             }
 
             return true;
         }
 
-        public boolean existWall() {
-            if (matrix[this.row][this.col] == '*') {
+        public boolean isEdge() {
+            if (this.row == 0 || this.row == N - 1 || this.col == 0 || this.col == M - 1) {
                 return true;
             }
 
             return false;
         }
 
-
-
+        public char getValue() {
+            return mainMatrix[this.row][this.col];
+        }
 
         @Override
         public boolean equals(Object obj) {
-            if (this == obj) { return true; }
-            if (obj != null || this.getClass() != obj.getClass()) { return false; }
+            if (this == obj) { return true;}
+            if (obj == null || this.getClass() != obj.getClass()) { return false; }
+
             Pos anotherPos = (Pos) obj;
-
-            if (this.row == anotherPos.row && this.col == anotherPos.col) {
-                return true;
-            }
-
+            if (this.row == anotherPos.row && this.col == anotherPos.col) { return true; }
             return false;
         }
 
@@ -68,160 +62,123 @@ public class 열쇠 {
         }
     }
 
+
     public static void main(String[] args) throws Exception {
         StringTokenizer st = new StringTokenizer(br.readLine());
-        T = Integer.parseInt(st.nextToken());
+        int T = Integer.parseInt(st.nextToken());
+
+
         for (int i = 0; i < T; i++) {
             init();
             solution();
         }
     }
 
-    public static void solution() throws Exception {
-        gatherStartPos();
-
+    public static void solution() {
         int answer = 0;
+        Map<Character, List<Pos>> blockedListMap = new HashMap<>();
+        String alphabets = "qwertyuiopasdfghjklzxcvbnm";
+        for (int i = 0; i < alphabets.length(); i++) {
+            blockedListMap.put(alphabets.charAt(i), new ArrayList<>());
+        }
+
+
+        Queue<Pos> queue = new ArrayDeque<>();
+        List<Pos> edgePosList = new ArrayList<>();
+        for (int i = 0; i < M; i++) {
+            edgePosList.add(new Pos(0, i));
+            edgePosList.add(new Pos(N - 1, i));
+        }
+
+        for (int i = 1; i < N - 1; i++) {
+            edgePosList.add(new Pos(i, 0));
+            edgePosList.add(new Pos(i, M - 1));
+        }
+
+
+        for (Pos pos : edgePosList) {
+            char value = mainMatrix[pos.row][pos.col];
+
+            if (value != '*' && !Character.isUpperCase(value)) {
+                queue.add(pos);
+                continue;
+            }
+
+            if (Character.isUpperCase(value) && keyMap.get(Character.toLowerCase(value)) == null) {
+                blockedListMap.get(Character.toLowerCase(value)).add(pos);
+            }
+
+            else if (Character.isUpperCase(value) && keyMap.get(Character.toLowerCase(value)) != null) {
+                queue.add(pos);
+            }
+        }
+
+
+        int[][] visited = new int[N][M];
         while (!queue.isEmpty()) {
-            Pos curPos = queue.pollLast();
+            Pos curPos = queue.poll();
+            if (visited[curPos.row][curPos.col] == 1) { continue; }
+            visited[curPos.row][curPos.col] = 1;
 
-            if (visited[curPos.row][curPos.col] == 1) {
-                continue;
-            }
-
-            char posValue = matrix[curPos.row][curPos.col];
-            if (Character.isLetter(posValue) && Character.isUpperCase(posValue)) {
-                blockedPosMap.get(posValue).add(new Pos(curPos.row, curPos.col));
-                continue;
-            }
-
-            else if (Character.isLetter(posValue) && Character.isLowerCase(posValue)) {
-                matrix[curPos.row][curPos.col] = '.';
-                openDoorsByKey(posValue);
-                addClosedDoorToQueue(posValue);
-                visited[curPos.row][curPos.col] = 1;
-            }
-
-            else if (matrix[curPos.row][curPos.col] == '$') {
-                matrix[curPos.row][curPos.col] = '.';
+            if (curPos.getValue() == '$') {
                 answer += 1;
-                visited[curPos.row][curPos.col] = 1;
-            }
-            else if (matrix[curPos.row][curPos.col] == '.') {
-                visited[curPos.row][curPos.col] = 1;
             }
 
+            else if (Character.isLowerCase(curPos.getValue())) {
+                keyMap.put(curPos.getValue(), 1);
+                queue.addAll(blockedListMap.get(curPos.getValue()));
+
+                blockedListMap.put(curPos.getValue(), new ArrayList<>());
+            }
+
+            else if (Character.isUpperCase(curPos.getValue()) && keyMap.get(Character.toLowerCase(curPos.getValue())) == null) {
+                blockedListMap.get(Character.toLowerCase(curPos.getValue())).add(curPos);
+            }
 
             for (Pos direction : directions) {
                 Pos movedPos = curPos.addPos(direction);
-                if (!movedPos.checkPosAvail() || movedPos.existWall()) {
-                    continue;
-                }
+                if (!movedPos.isValidIndex()) { continue; }
+                if (movedPos.getValue() == '*') { continue; }
+                if (visited[movedPos.row][movedPos.col] == 1) { continue; }
 
-                if (visited[movedPos.row][movedPos.col] == 1) {
+                if (Character.isUpperCase(movedPos.getValue()) && keyMap.get(Character.toLowerCase(movedPos.getValue())) == null) {
+                    blockedListMap.get(Character.toLowerCase(movedPos.getValue())).add(movedPos);
                     continue;
                 }
 
                 queue.add(movedPos);
             }
         }
+
         System.out.println(answer);
-        return;
-    }
-
-    public static void addClosedDoorToQueue(char key) {
-        char door = Character.toUpperCase(key);
-        for (Pos closedDoorPos : blockedPosMap.get(door)) {
-            queue.add(closedDoorPos);
-        }
-    }
-
-    public static void gatherStartPos() {
-        for (int j = 0; j < col; j++) {
-            if (matrix[0][j] != '*' && !isUpperLetter(matrix[0][j])) {
-                queue.add(new Pos(0, j));
-            }
-            if (isUpperLetter(matrix[0][j])) {
-                blockedPosMap.get(matrix[0][j]).add(new Pos(0, j));
-            }
-
-            if (matrix[row - 1][j] != '*' && !isUpperLetter(matrix[row - 1][j])) {
-                queue.add(new Pos(row - 1, j));
-            }
-            if (isUpperLetter(matrix[row - 1][j])) {
-                blockedPosMap.get(matrix[row - 1][j]).add(new Pos(row - 1, j));
-            }
-
-        }
-
-        for (int i = 1; i < row - 1; i++) {
-            if (matrix[i][0] != '*' && !isUpperLetter(matrix[i][0])) {
-                queue.add(new Pos(i, 0));
-            }
-            if (isUpperLetter(matrix[i][0])) {
-                blockedPosMap.get(matrix[i][0]).add(new Pos(i, 0));
-            }
-
-            if (matrix[i][col - 1] != '*' && !isUpperLetter(matrix[i][col - 1])) {
-                queue.add(new Pos(i, col - 1));
-            }
-            if (isUpperLetter(matrix[i][col - 1])) {
-                blockedPosMap.get(matrix[i][col - 1]).add(new Pos(i, col - 1));
-            }
-        }
-    }
-
-    public static void openDoorsByKey(char key) {
-        char destDoor = Character.toUpperCase(key);
-        for (Pos doorPos : doorMap.get(destDoor)) {
-            matrix[doorPos.row][doorPos.col] = '.';
-        }
-
-        doorMap.put(destDoor, new ArrayList<>());
-    }
-
-    public static boolean isUpperLetter(char x) {
-        if (Character.isLetter(x) && Character.isUpperCase(x)) {
-            return true;
-        }
-
-        return false;
     }
 
 
     public static void init() throws IOException {
-        doorMap = new HashMap<>();
-        blockedPosMap = new HashMap<>();
-        for (int i = 0; i < doorString.length(); i++) {
-            doorMap.put(doorString.charAt(i), new ArrayList<>());
-            blockedPosMap.put(doorString.charAt(i), new ArrayList<>());
-        }
-
         StringTokenizer st = new StringTokenizer(br.readLine());
-        row = Integer.parseInt(st.nextToken());
-        col = Integer.parseInt(st.nextToken());
 
-        queue = new ArrayDeque<>();
-        matrix = new char[row][col];
-        visited = new int[row][col];
+        N = Integer.parseInt(st.nextToken());
+        M = Integer.parseInt(st.nextToken());
 
-        for (int i = 0; i < row; i++) {
+        mainMatrix = new char[N][M];
+        for (int i = 0; i < N; i++) {
             st = new StringTokenizer(br.readLine());
-            String newRow = st.nextToken();
-            for (int j = 0; j < col; j++) {
-                matrix[i][j] = newRow.charAt(j);
-                if (Character.isLetter(matrix[i][j]) && !Character.isLowerCase(matrix[i][j])) {
-                    doorMap.get(matrix[i][j]).add(new Pos(i, j));
-                }
+            String string = st.nextToken();
+
+            for (int j = 0; j < M; j++) {
+                mainMatrix[i][j] = string.charAt(j);
             }
         }
 
+        keyMap = new HashMap<>();
         st = new StringTokenizer(br.readLine());
-        String availableKeyString = st.nextToken();
-        if (!availableKeyString.equals("0")) {
-            for (int i = 0; i < availableKeyString.length(); i++) {
-                openDoorsByKey(availableKeyString.charAt(i));
-            }
+        String string = st.nextToken();
+        for (int i = 0; i < string.length(); i++) {
+            char key = string.charAt(i);
+            keyMap.put(key, 1);
         }
-    }
 
+
+
+    }
 }
