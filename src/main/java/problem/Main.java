f8917f492b6b42c03fdb806d44596d59ed1ba0b0
윤도경 <youndokyung@yundogyeong-ui-Macmini.local>
2026-04-05 17:55:53 +0900

    트리와쿼리 solved

--- src/main/java/problem/Main.java
+++ src/main/java/problem/Main.java
@@ -5,8 +5,19 @@ import java.io.*;
 
 
 public class Main {
-    static int N;
-    static int[] numList;
+    static int N, R, Q;
+    static int[] queryList;
+    static Map<Integer, List<Integer>> graphMap;
+
+    public static class Node {
+        int num;
+        int numNode;
+
+        public Node(int num, int numNode) {
+            this.num = num;
+            this.numNode = numNode;
+        }
+    }
 
     public static void main(String[] args) throws Exception {
         init();
@@ -14,35 +25,60 @@ public class Main {
     }
 
     public static void solution() {
-        int answerLeftNum = 0;
-        int answerRightNum = 0;
+        int[] visited = new int[N];
+        int[] numChildList = new int[N];
 
-        int leftIndex = 0;
-        int rightIndex = N - 1;
+        Deque<Integer> queue = new ArrayDeque<>();
+        queue.add(R);
 
-        int minSumValue = Integer.MAX_VALUE;
+        while (!queue.isEmpty()) {
+            int num = queue.pollFirst();
+            int numChild = 0;
+            visited[num] = 1;
 
-        while (leftIndex < rightIndex) {
-            int sumValue = numList[leftIndex] + numList[rightIndex];
+            for (int nearNum : graphMap.get(num)) {
+                if (visited[nearNum] == 1) { continue; }
 
-            if (Math.abs(sumValue) < minSumValue) {
-                minSumValue = Math.abs(sumValue);
-
-                answerLeftNum = numList[leftIndex];
-                answerRightNum = numList[rightIndex];
+                numChild += 1;
+                queue.add(nearNum);
             }
 
+            numChildList[num] = numChild;
+        }
 
-            if (sumValue < 0) {
-                leftIndex += 1;
-            }
-
-            else {
-                rightIndex -= 1;
+        queue = new ArrayDeque<>();
+        for (int i = 0; i < N; i++) {
+            if (numChildList[i] == 0) {
+                queue.add(i);
             }
         }
 
-        System.out.println(answerLeftNum + " " + answerRightNum);
+        int[] answerList = new int[N];
+        visited = new int[N];
+
+        while (!queue.isEmpty()) {
+            int num = queue.pollFirst();
+            visited[num] = 1;
+            answerList[num] += 1;
+
+            for (int parentNum : graphMap.get(num)) {
+                if (visited[parentNum] == 1) { continue; }
+
+                answerList[parentNum] += answerList[num];
+                numChildList[parentNum] -= 1;
+
+                if (numChildList[parentNum] == 0) {
+                    queue.add(parentNum);
+                }
+            }
+        }
+
+        StringBuilder sb = new StringBuilder();
+        for (int i = 0; i < Q; i++) {
+            sb.append(answerList[queryList[i]] + "\n");
+        }
+
+        System.out.println(sb.toString().substring(0, sb.length() - 1));
     }
 
     public static void init() throws IOException {
@@ -50,14 +86,35 @@ public class Main {
         StringTokenizer st = new StringTokenizer(br.readLine());
 
         N = Integer.parseInt(st.nextToken());
-        numList = new int[N];
+        R = Integer.parseInt(st.nextToken()) - 1;
+        Q = Integer.parseInt(st.nextToken());
 
-        st = new StringTokenizer(br.readLine());
+        graphMap = new HashMap<>();
         for (int i = 0; i < N; i++) {
-            numList[i] = Integer.parseInt(st.nextToken());
+            graphMap.put(i, new ArrayList<>());
         }
 
-        Arrays.sort(numList);
+        for (int i = 0; i < N - 1; i++) {
+            st = new StringTokenizer(br.readLine());
+
+            int node1 = Integer.parseInt(st.nextToken()) - 1;
+            int node2 = Integer.parseInt(st.nextToken()) - 1;
+
+
+            graphMap.get(node1).add(node2);
+            graphMap.get(node2).add(node1);
+        }
+
+        queryList = new int[Q];
+        for (int i = 0; i < Q; i++) {
+            st = new StringTokenizer(br.readLine());
+
+            queryList[i] = Integer.parseInt(st.nextToken()) - 1;
+        }
+
+
     }
+
+
 
 }
--- src/main/java/problem/트리와쿼리.java
+++ src/main/java/problem/트리와쿼리.java
@@ -1,14 +1,24 @@
 package problem;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 import java.util.*;
-import java.io.*;
-
-
 
 public class 트리와쿼리 {
     static int N, R, Q;
-    static int[] numPointedList, queryList, answerList, visitedList;
+    static int[] queryList;
     static Map<Integer, List<Integer>> graphMap;
+
+    public static class Node {
+        int num;
+        int numNode;
+
+        public Node(int num, int numNode) {
+            this.num = num;
+            this.numNode = numNode;
+        }
+    }
 
     public static void main(String[] args) throws Exception {
         init();
@@ -16,34 +26,60 @@ public class 트리와쿼리 {
     }
 
     public static void solution() {
+        int[] visited = new int[N];
+        int[] numChildList = new int[N];
+
+        Deque<Integer> queue = new ArrayDeque<>();
+        queue.add(R);
+
+        while (!queue.isEmpty()) {
+            int num = queue.pollFirst();
+            int numChild = 0;
+            visited[num] = 1;
+
+            for (int nearNum : graphMap.get(num)) {
+                if (visited[nearNum] == 1) { continue; }
+
+                numChild += 1;
+                queue.add(nearNum);
+            }
+
+            numChildList[num] = numChild;
+        }
+
+        queue = new ArrayDeque<>();
+        for (int i = 0; i < N; i++) {
+            if (numChildList[i] == 0) {
+                queue.add(i);
+            }
+        }
+
+        int[] answerList = new int[N];
+        visited = new int[N];
+
+        while (!queue.isEmpty()) {
+            int num = queue.pollFirst();
+            visited[num] = 1;
+            answerList[num] += 1;
+
+            for (int parentNum : graphMap.get(num)) {
+                if (visited[parentNum] == 1) { continue; }
+
+                answerList[parentNum] += answerList[num];
+                numChildList[parentNum] -= 1;
+
+                if (numChildList[parentNum] == 0) {
+                    queue.add(parentNum);
+                }
+            }
+        }
+
         StringBuilder sb = new StringBuilder();
-
-        recursive(R);
-
-        for (int query : queryList) {
-            int answer = recursive(query);
-            sb.append(answer + "\n");
+        for (int i = 0; i < Q; i++) {
+            sb.append(answerList[queryList[i]] + "\n");
         }
 
         System.out.println(sb.toString().substring(0, sb.length() - 1));
-    }
-
-    public static int recursive(int num) {
-        if (answerList[num] != 0) {
-            return answerList[num];
-        }
-
-        visitedList[num] = 1;
-        int answer = 1;
-
-        for (int nearNode : graphMap.get(num)) {
-            if (visitedList[nearNode] == 1) { continue; }
-
-            answer += recursive(nearNode);
-        }
-
-        answerList[num] = answer;
-        return answer;
     }
 
     public static void init() throws IOException {
@@ -54,7 +90,6 @@ public class 트리와쿼리 {
         R = Integer.parseInt(st.nextToken()) - 1;
         Q = Integer.parseInt(st.nextToken());
 
-        numPointedList = new int[N];
         graphMap = new HashMap<>();
         for (int i = 0; i < N; i++) {
             graphMap.put(i, new ArrayList<>());
@@ -66,11 +101,9 @@ public class 트리와쿼리 {
             int node1 = Integer.parseInt(st.nextToken()) - 1;
             int node2 = Integer.parseInt(st.nextToken()) - 1;
 
+
             graphMap.get(node1).add(node2);
             graphMap.get(node2).add(node1);
-
-            numPointedList[node1] += 1;
-            numPointedList[node2] += 1;
         }
 
         queryList = new int[Q];
@@ -80,8 +113,9 @@ public class 트리와쿼리 {
             queryList[i] = Integer.parseInt(st.nextToken()) - 1;
         }
 
-        answerList = new int[N];
-        visitedList = new int[N];
+
     }
+
+
 
 }
