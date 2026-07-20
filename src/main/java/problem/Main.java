a22e150ffdde6b61b385eb0b0dde7f12ea6f1884
윤도경 <youndokyung@yundogyeong-ui-Macmini.local>
2026-07-21 08:05:23 +0900

    줄세우기 solved

--- src/main/java/problem/Main.java
+++ src/main/java/problem/Main.java
@@ -5,7 +5,9 @@ import java.io.*;
 
 
 public class Main {
-    static int N;
+    static int N, M;
+    static Map<Integer, List<Integer>> graphMap;
+    static int[] numPointedList;
 
     public static void main(String[] args) throws Exception {
         init();
@@ -13,51 +15,31 @@ public class Main {
     }
 
     public static void solution() {
-        int[] isPrime = new int[N + 1];
-        Arrays.fill(isPrime, 1);
-        List<Integer> primeList = new ArrayList<>();
+        ArrayDeque<Integer> queue = new ArrayDeque<>();
+        for (int i = 0; i < N; i++) {
+            if (numPointedList[i] != 0) { continue; }
 
-        for (int i = 2; i < N + 1; i++) {
-            if (isPrime[i] == 0) { continue; }
-            primeList.add(i);
-
-            for (int num = i * 2; num < N + 1; num += i) {
-                isPrime[num] = 0;
-            }
+            queue.add(i);
         }
 
 
-        List<Integer> sumList = new ArrayList<>();
-        sumList.add(0);
-        for (int i = 0; i < primeList.size(); i++) {
-            sumList.add(sumList.get(sumList.size() - 1) + primeList.get(i));
-        }
+        StringBuilder sb = new StringBuilder();
+        while (!queue.isEmpty()) {
+            int num = queue.pollFirst();
+            sb.append(num + 1);
+            sb.append(" ");
 
 
-        int answer = 0;
+            for (int nearNum : graphMap.get(num)) {
+                numPointedList[nearNum] -= 1;
 
-        int leftIndex = 0;
-        int rightIndex = 1;
-        while (rightIndex < sumList.size()) {
-            int sumValue = sumList.get(rightIndex) - sumList.get(leftIndex);
-
-            if (sumValue == N) {
-                answer += 1;
-                leftIndex += 1;
-            }
-
-            if (sumValue < N) {
-                rightIndex += 1;
-                continue;
-            }
-
-            if (sumValue > N) {
-                leftIndex += 1;
-                continue;
+                if (numPointedList[nearNum] == 0) {
+                    queue.add(nearNum);
+                }
             }
         }
 
-        System.out.println(answer);
+        System.out.println(sb.toString().substring(0, sb.length() - 1));
     }
 
     public static void init() throws IOException {
@@ -65,5 +47,25 @@ public class Main {
         StringTokenizer st = new StringTokenizer(br.readLine());
 
         N = Integer.parseInt(st.nextToken());
+        M = Integer.parseInt(st.nextToken());
+
+
+        numPointedList = new int[N];
+
+        graphMap = new HashMap<>();
+        for (int i = 0; i < N; i++) {
+            graphMap.put(i, new ArrayList<>());
+        }
+
+
+        for (int i = 0; i < M; i++) {
+            st = new StringTokenizer(br.readLine());
+
+            int from = Integer.parseInt(st.nextToken()) - 1;
+            int to = Integer.parseInt(st.nextToken()) - 1;
+
+            graphMap.get(from).add(to);
+            numPointedList[to] += 1;
+        }
     }
 }
--- src/main/java/problem/줄세우기.java
+++ src/main/java/problem/줄세우기.java
@@ -3,10 +3,11 @@ package problem;
 import java.util.*;
 import java.io.*;
 
+
 public class 줄세우기 {
     static int N, M;
-    static int[] numPointedList;
     static Map<Integer, List<Integer>> graphMap;
+    static int[] numPointedList;
 
     public static void main(String[] args) throws Exception {
         init();
@@ -14,9 +15,7 @@ public class 줄세우기 {
     }
 
     public static void solution() {
-        StringBuilder sb = new StringBuilder();
-
-        Deque<Integer> queue = new ArrayDeque<>();
+        ArrayDeque<Integer> queue = new ArrayDeque<>();
         for (int i = 0; i < N; i++) {
             if (numPointedList[i] != 0) { continue; }
 
@@ -24,16 +23,19 @@ public class 줄세우기 {
         }
 
 
+        StringBuilder sb = new StringBuilder();
         while (!queue.isEmpty()) {
             int num = queue.pollFirst();
             sb.append(num + 1);
             sb.append(" ");
 
+
             for (int nearNum : graphMap.get(num)) {
                 numPointedList[nearNum] -= 1;
-                if (numPointedList[nearNum] != 0) { continue; }
 
-                queue.add(nearNum);
+                if (numPointedList[nearNum] == 0) {
+                    queue.add(nearNum);
+                }
             }
         }
 
@@ -47,11 +49,14 @@ public class 줄세우기 {
         N = Integer.parseInt(st.nextToken());
         M = Integer.parseInt(st.nextToken());
 
+
         numPointedList = new int[N];
+
         graphMap = new HashMap<>();
         for (int i = 0; i < N; i++) {
             graphMap.put(i, new ArrayList<>());
         }
+
 
         for (int i = 0; i < M; i++) {
             st = new StringTokenizer(br.readLine());
@@ -59,8 +64,8 @@ public class 줄세우기 {
             int from = Integer.parseInt(st.nextToken()) - 1;
             int to = Integer.parseInt(st.nextToken()) - 1;
 
-            numPointedList[to] += 1;
             graphMap.get(from).add(to);
+            numPointedList[to] += 1;
         }
     }
 }
