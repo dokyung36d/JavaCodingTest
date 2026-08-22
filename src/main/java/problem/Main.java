737aca626348e3d943cd2a6357bdfa65ccc35bf8
윤도경 <youndokyung@yundogyeong-ui-Macmini.local>
2026-08-22 09:46:31 +0900

    1의 개수세기 solved

--- src/main/java/problem/Main.java
+++ src/main/java/problem/Main.java
@@ -5,100 +5,45 @@ import java.io.*;
 
 
 public class Main {
-    static int N, K;
-    static PriorityQueue<Gem> gemPq;
-    static PriorityQueue<PolledGem> polledGemPq;
-    static int[] bagList;
-
-    public static class Gem implements Comparable<Gem> {
-        int m;
-        int v;
-
-        public Gem(int m, int v) {
-            this.m = m;
-            this.v = v;
-        }
-
-        @Override
-        public int compareTo(Gem anotherGem) {
-            return Integer.compare(this.m, anotherGem.m);
-        }
-    }
-
-    public static class PolledGem implements Comparable<PolledGem> {
-        int m;
-        int v;
-
-        public PolledGem(int m, int v) {
-            this.m = m;
-            this.v = v;
-        }
-
-        @Override
-        public int compareTo(PolledGem anotherPolledGem) {
-            return Integer.compare(-this.v, -anotherPolledGem.v);
-        }
-    }
-
+    static long A, B;
+    static List<Long> dpList;
+    
     public static void main(String[] args) throws Exception {
         init();
         solution();
     }
-
+    
     public static void solution() {
-        long answer = (long) 0;
+        dpList = new ArrayList<>();
+        dpList.add((long) 1);
 
-        for (int bag : bagList) {
-            while (!gemPq.isEmpty()) {
-                Gem gem = gemPq.poll();
-
-                if (gem.m > bag) {
-                    gemPq.add(gem);
-                    break;
-                }
-
-                polledGemPq.add(new PolledGem(gem.m, gem.v));
-            }
-
-
-            if (!polledGemPq.isEmpty()) {
-                PolledGem polledGem = polledGemPq.poll();
-
-                answer += (long) polledGem.v;
-            }
+        while (Math.pow(2, dpList.size()) < B) {
+            dpList.add((long) Math.pow(2, dpList.size()) + (long) 2 * dpList.get(dpList.size() - 1));
         }
 
 
-        System.out.println(answer);
+        long bNum = recursive(B);
+        long aNum = recursive(A - 1);
+        System.out.println((long) recursive(B) - (long) recursive(A - 1));
     }
 
+
+    public static long recursive(long num) {
+        int logValue = (int) (Math.log(num) / Math.log(2));
+        if (num == 0) { return 0; }
+        if (num == 1) { return 1; }
+        if (Math.pow(2, logValue) == num) { return 1 + dpList.get(logValue - 1); }
+
+
+        return dpList.get(logValue - 1) + (long) recursive((long) (num - (long) Math.pow(2, logValue))) + (long) (num - Math.pow(2, logValue) + 1);
+    }
+
+    
     public static void init() throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine());
-
-        N = Integer.parseInt(st.nextToken());
-        K = Integer.parseInt(st.nextToken());
-
-        gemPq = new PriorityQueue<>();
-        polledGemPq = new PriorityQueue<>();
-        for (int i = 0; i < N; i++) {
-            st = new StringTokenizer(br.readLine());
-
-            int m = Integer.parseInt(st.nextToken());
-            int v = Integer.parseInt(st.nextToken());
-
-
-            gemPq.add(new Gem(m, v));
-        }
-
-
-        bagList = new int[K];
-        for (int i = 0; i < K; i++) {
-            st = new StringTokenizer(br.readLine());
-
-            bagList[i] = Integer.parseInt(st.nextToken());
-        }
-
-        Arrays.sort(bagList);
+        
+        A = Long.parseLong(st.nextToken());
+        B = Long.parseLong(st.nextToken());
     }
 }
--- src/main/java/problem/One의개수세기.java
+++ src/main/java/problem/One의개수세기.java
@@ -4,9 +4,8 @@ import java.util.*;
 import java.io.*;
 
 
-
 public class One의개수세기 {
-    static long a, b;
+    static long A, B;
     static List<Long> dpList;
 
     public static void main(String[] args) throws Exception {
@@ -18,53 +17,33 @@ public class One의개수세기 {
         dpList = new ArrayList<>();
         dpList.add((long) 1);
 
-        while (Math.pow(2, dpList.size()) < b) {
-            long dpNum = 2 * dpList.get(dpList.size() - 1) + (long) Math.pow(2, dpList.size());
-            dpList.add(dpNum);
+        while (Math.pow(2, dpList.size()) < B) {
+            dpList.add((long) Math.pow(2, dpList.size()) + (long) 2 * dpList.get(dpList.size() - 1));
         }
 
-        long aNumOne = getNumOne(a);
-        long bNumOne = getNumOne(b);
-        System.out.println(bNumOne - aNumOne);
+
+        long bNum = recursive(B);
+        long aNum = recursive(A - 1);
+        System.out.println((long) recursive(B) - (long) recursive(A - 1));
     }
 
-    public static long getNumOne(long num) {
-        if (num == (long) 1) {
-            return 1;
-        }
+
+    public static long recursive(long num) {
+        int logValue = (int) (Math.log(num) / Math.log(2));
+        if (num == 0) { return 0; }
+        if (num == 1) { return 1; }
+        if (Math.pow(2, logValue) == num) { return 1 + dpList.get(logValue - 1); }
 
 
-        long numOne = (long) 0;
-
-        int curBit = 63 - Long.numberOfLeadingZeros(num);
-
-        while (num > (long) 0) {
-            if (num <= (long) 2) {
-                numOne += num;
-                break;
-            }
-
-
-            long isOne = (num & ((long) 1 << curBit));
-            if (isOne == (long) 0) {
-                curBit -= 1;
-                continue;
-            }
-
-            numOne += dpList.get(curBit - 1);
-            num -= ((long) 1 << curBit);
-            numOne += (num + 1);
-            curBit -= 1;
-        }
-
-        return numOne;
+        return dpList.get(logValue - 1) + (long) recursive((long) (num - (long) Math.pow(2, logValue))) + (long) (num - Math.pow(2, logValue) + 1);
     }
+
 
     public static void init() throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine());
 
-        a = Long.parseLong(st.nextToken()) - 1;
-        b = Long.parseLong(st.nextToken());
+        A = Long.parseLong(st.nextToken());
+        B = Long.parseLong(st.nextToken());
     }
 }
